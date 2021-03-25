f5815bcd9967cea4b2332b70d95b8463a1797435
webdozerz <webdozerz@gmail.com>
2021-03-25 11:13:31 +0700

    [update] Confirm

--- pages/confirm.vue
+++ pages/confirm.vue
@@ -4,10 +4,12 @@
 
 <script>
 export default {
+  layout: 'confirm',
   async mounted() {
     await this.$store.dispatch('user/confirm', {
       confirmCode: this.$route.query.token,
     });
+    this.$store.dispatch('main/showInfo', 'Email confirmed successfully');
     this.$router.push('/sign-in');
   },
 };
