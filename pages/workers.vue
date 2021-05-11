a781fa0b49c293970cd4ac0169a954cd7ac896ef
ghost-raven1 <sonofagronom@gmail.com>
2021-05-11 18:36:04 +0700

    some fixes.

--- pages/workers.vue
+++ pages/workers.vue
@@ -1,50 +1,46 @@
 <template>
   <div>
     <div class="map__container">
-      <transition name="fade-fast">
-        <GMap
-          v-if="isShowMap"
-          ref="gMap"
-          class="quests__map"
-          language="en"
-          :center="{lat: locations[0].lat, lng: locations[0].lng}"
-          :zoom="6"
-        />
-      </transition>
-      <div class="main__searchBar">
-        <div
-          id="searchBar"
-          class="search-bar"
-        >
-          <div class="search-bar__body">
-            <div class="checkbox__isShowMap">
-              <input
-                id="isShowMap"
+      <div class="quests__top">
+        <transition name="fade-fast">
+          <GMap
+            v-if="isShowMap"
+            ref="gMap"
+            class="quests__map"
+            language="en"
+            :center="{lat: locations[0].lat, lng: locations[0].lng}"
+            :zoom="6"
+          />
+        </transition>
+        <div class="quests__search">
+          <div class="search">
+            <div class="search__toggle">
+              <base-checkbox
                 v-model="isShowMap"
-                type="checkbox"
-                class="checkbox-input"
-              >
-              <label
-                class="checkbox__label"
-                for="isShowMap"
-              > Show map </label>
+                name="map"
+                :label="$t('quests.ui.showMap')"
+              />
             </div>
-            <input
-              type="text"
-              class="ctm-field_search search-bar__input"
-              placeholder="City / Street / Place"
-            >
-            <!--              TODO: Style a button-->
-            <button class="header__button header__button_locale search-bar__btn">
-              <span class=""> +500m </span>
-              <span class="icon-caret_down" />
-            </button>
-            <base-btn
-              class="message__action search-bar__btn-search"
-              @click="toggleMap()"
-            >
-              Search workers
-            </base-btn>
+            <div class="search__inputs">
+              <base-field
+                v-model="search"
+                is-search
+                class="search__input"
+                :placeholder="$t('quests.ui.search')"
+                :mode="'icon'"
+              />
+            </div>
+            <div class="search__dd">
+              <base-dd
+                v-model="distanceIndex"
+                :items="distance"
+              />
+            </div>
+            <div class="search__actions">
+              <base-btn class="search__btn">
+                Search quests
+              </base-btn>
+            </div>
           </div>
         </div>
       </div>
@@ -448,12 +444,108 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.quests {
+  &__cards {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-gap: 20px;
+    padding-top: 20px;
+  }
+  &__top {
+    position: relative;
+    min-height: 160px;
+  }
+  &__search {
+    position: absolute;
+    max-width: 1180px;
+    height: 83px;
+    bottom: 30px;
+    left: 0;
+    right: 0;
+    margin: auto;
+    z-index: 1200;
+    @include box;
+  }
+  &__content {
+    display: flex;
+    justify-content: center;
+  }
+  &__body {
+    padding-top: 30px;
+    max-width: 1180px;
+    width: 100%;
+    height: 100%;
+    &_wrap {
+      padding-top: 10px;
+    }
+  }
+  &__text {
+    font-family: 'Inter', sans-serif;
+    font-style: normal;
+    &_title  {
+      font-weight: 500;
+      font-size: 25px;
+      line-height: 130%;
+      color: $black800;
+    }
+  }
+  &__tags {
+    padding-top: 30px;
+    max-width: 1180px;
+  }
+  &__tools {
+    padding-top:  20px;
+  }
+}
+.search {
+  display: grid;
+  grid-template-columns: 154px 1fr 143px 260px;
+  align-items: center;
+  height: 100%;
+  justify-items: center;
+  &__dd {
+    border-left: 1px solid #F7F8FA;
+    height: 100%;
+  }
+  &__icon {
+    margin-bottom: -10px;
+    &::before {
+      font-size: 24px;
+      color: $blue;
+    }
+  }
+  &__inputs {
+    padding: 0 20px;
+    width: 100%;
+    display: grid;
+    align-items: center;
+  }
+  &__input {
+    display: flex;
+    align-items: center;
+  }
+  &__btn {
+    max-width: 220px;
+  }
+  &__toggle {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-right: 1px solid #F7F8FA;
+  }
+  &__actions {
+    height: 100%;
+    border-left: 1px solid #F7F8FA;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+  }
+}
 .main {
   @include main;
-  &__searchBar {
-    justify-items: center;
-    position: relative;
-  }
   &-white {
     @include main-white;
   }
