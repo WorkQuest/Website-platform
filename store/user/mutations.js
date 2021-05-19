37a33b3fe2f3f8904f3f8055f9e92e398b3e3344
iliyaandrushov <webdozerz@gmail.com>
2021-05-19 08:55:07 +0300

    [update] Mutations

--- store/user/mutations.js
+++ store/user/mutations.js
@@ -4,6 +4,8 @@ export default {
   setOldTokens(state, { access, refresh }) {
     state.tokens.access = access;
     state.tokens.refresh = refresh;
+    this.$cookies.set('access', access, { path: '/' });
+    this.$cookies.set('refresh', refresh, { path: '/' });
   },
   setNewTokens(state, { access, refresh }) {
     state.tokens.access = access;
