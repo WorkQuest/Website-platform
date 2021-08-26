a391213d8bef44b1874e0253f1238f170fe44afb
ghost-raven1 <sonofagronom@gmail.com>
2021-08-26 14:57:47 +0700

    hotfix/10099 deleted raising-views page, replaced all func on create-quest page.

--- pages/create-quest.vue
+++ pages/create-quest.vue
@@ -1,10 +1,16 @@
 <template>
-  <div class="main main-white">
+  <div
+    class="main"
+    :class="{'main-white': step === 1}"
+  >
     <div class="main__body page">
       <validation-observer
         v-slot="{handleSubmit, validated, passed, invalid}"
       >
-        <div class="page">
+        <div
+          v-if="step === 1"
+          class="page"
+        >
           <h2 class="page__title">
             {{ $t('quests.createAQuest') }}
           </h2>
@@ -198,10 +204,219 @@
             <div class="btn__create">
               <base-btn
                 :disabled="!validated || !passed || invalid"
-                @click="handleSubmit(showQuestCreatedModal)"
+                @click="handleSubmit(toRiseViews)"
               >
                 {{ $t('quests.createAQuest') }}
               </base-btn>
+            </div>
+          </div>
+        </div>
+        <div
+          v-if="step === 2"
+          class="page"
+        >
+          <div class="page btn-container__left">
+            <div class="btn-container__btn_back">
+              <base-btn
+                :mode="'back'"
+                @click="goBack()"
+              >
+                {{ $t('meta.back') }}
+                <template v-slot:left>
+                  <span class="icon-chevron_big_left" />
+                </template>
+              </base-btn>
+            </div>
+          </div>
+          <div class="page page__raising">
+            {{ $t('raising-views.raisingViews') }}
+          </div>
+          <div class="page period">
+            <h3 class="period__choose">
+              {{ $t('raising-views.choosePeriod') }}
+            </h3>
+            <div class="period__container">
+              <div
+                class="period__period"
+                :class="{'period__period_active': period === 1}"
+                @click="toFirstPeriod"
+              >
+                <h2
+                  class="period__title"
+                  :class="{'period__title_active': period === 1}"
+                >
+                  {{ $t('raising-views.forOneDay') }}
+                </h2>
+              </div>
+
+              <div
+                class="period__period"
+                :class="{'period__period_active': period === 2}"
+                @click="toSecondPeriod"
+              >
+                <h2
+                  class="period__title"
+                  :class="{'period__title_active': period === 2}"
+                >
+                  {{ $t('raising-views.forOneWeek') }}
+                </h2>
+              </div>
+
+              <div
+                class="period__period"
+                :class="{'period__period_active': period === 3}"
+                @click="toThirdPeriod"
+              >
+                <h3
+                  class="period__title"
+                  :class="{'period__title_active': period === 3}"
+                >
+                  {{ $t('raising-views.forOneMonth') }}
+                </h3>
+              </div>
+            </div>
+
+            <div class="period level">
+              <div class="level__title">
+                {{ $t('raising-views.chooseLevel') }}
+              </div>
+              <div
+                v-if="period === 1"
+                class="level__container"
+              >
+                <div
+                  v-for="(item, i) in levels1Day"
+                  :key="i"
+                  class="level__card"
+                  :class="{'level__card_gold': item.code === 1}"
+                  @click="selectRadio(i)"
+                >
+                  <div class="level__option">
+                    <input
+                      :ref="`radio${i}`"
+                      name="higherLevel"
+                      type="radio"
+                      class="radio__input"
+                    >
+                  </div>
+                  <div class="level card">
+                    <div
+                      class="card__level"
+                      :class="[
+                        {'card__level_reliable': item.code === 3},
+                        {'card__level_checked': item.code === 4}
+                      ]"
+                    >
+                      {{ item.level }}
+                    </div>
+                    <div class="card__desc">
+                      {{ item.desc }}
+                    </div>
+                  </div>
+                  <div class="cost__container">
+                    <div class="card__cost">
+                      {{ item.cost }}$
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div
+                v-if="period === 2"
+                class="level__container"
+              >
+                <div
+                  v-for="(item, i) in levels1Week"
+                  :key="i"
+                  class="level__card"
+                  :class="{'level__card_gold': item.code === 1}"
+                  @click="selectRadio(i)"
+                >
+                  <div class="level__option">
+                    <input
+                      :ref="`radio${i}`"
+                      name="higherLevel"
+                      type="radio"
+                      class="radio__input"
+                    >
+                  </div>
+                  <div class="level card">
+                    <div
+                      class="card__level"
+                      :class="[
+                        {'card__level_reliable': item.code === 3},
+                        {'card__level_checked': item.code === 4}
+                      ]"
+                    >
+                      {{ item.level }}
+                    </div>
+                    <div class="card__desc">
+                      {{ item.desc }}
+                    </div>
+                  </div>
+                  <div class="cost__container">
+                    <div class="card__cost">
+                      {{ item.cost }}$
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div
+                v-if="period === 3"
+                class="level__container"
+              >
+                <div
+                  v-for="(item, i) in levels1Month"
+                  :key="i"
+                  class="level__card"
+                  :class="{'level__card_gold': item.code === 1}"
+                  @click="selectRadio(i)"
+                >
+                  <div class="level__option">
+                    <input
+                      :ref="`radio${i}`"
+                      name="higherLevel"
+                      type="radio"
+                      class="radio__input"
+                    >
+                  </div>
+                  <div class="level card">
+                    <div
+                      class="card__level"
+                      :class="[
+                        {'card__level_reliable': item.code === 3},
+                        {'card__level_checked': item.code === 4}
+                      ]"
+                    >
+                      {{ item.level }}
+                    </div>
+                    <div class="card__desc">
+                      {{ item.desc }}
+                    </div>
+                  </div>
+                  <div class="cost__container">
+                    <div class="card__cost">
+                      {{ item.cost }}$
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="btn-container">
+              <div class="btn-container__btn">
+                <base-btn
+                  :mode="'outline'"
+                  @click="showQuestCreatedModal()"
+                >
+                  {{ $t('meta.skipAndEnd') }}
+                </base-btn>
+              </div>
+              <div class="btn-container__btn">
+                <base-btn
+                  @click="showPaymentModal"
+                >
+                  {{ $t('meta.pay') }}
+                </base-btn>
+              </div>
             </div>
           </div>
         </div>
@@ -230,6 +445,86 @@ export default {
   },
   data() {
     return {
+      step: 1,
+      period: 1,
+      levels1Day: [
+        {
+          level: this.$t('quests.levels.1.title'),
+          code: 1,
+          desc: this.$t('quests.levels.1.desc'),
+          cost: '10',
+        },
+        {
+          level: this.$t('quests.levels.2.title'),
+          code: 2,
+          desc: this.$t('quests.levels.2.desc'),
+          cost: '10',
+        },
+        {
+          level: this.$t('quests.levels.3.title'),
+          code: 3,
+          desc: this.$t('quests.levels.3.desc'),
+          cost: '10',
+        },
+        {
+          level: this.$t('quests.levels.4.title'),
+          code: 4,
+          desc: this.$t('quests.levels.4.desc'),
+          cost: '10',
+        },
+      ],
+      levels1Week: [
+        {
+          level: this.$t('quests.levels.1.title'),
+          code: 1,
+          desc: this.$t('quests.levels.1.desc'),
+          cost: '40',
+        },
+        {
+          level: this.$t('quests.levels.2.title'),
+          code: 2,
+          desc: this.$t('quests.levels.2.desc'),
+          cost: '10',
+        },
+        {
+          level: this.$t('quests.levels.3.title'),
+          code: 3,
+          desc: this.$t('quests.levels.3.desc'),
+          cost: '40',
+        },
+        {
+          level: this.$t('quests.levels.4.title'),
+          code: 4,
+          desc: this.$t('quests.levels.4.desc'),
+          cost: '40',
+        },
+      ],
+      levels1Month: [
+        {
+          level: this.$t('quests.levels.1.title'),
+          code: 1,
+          desc: this.$t('quests.levels.1.desc'),
+          cost: '70',
+        },
+        {
+          level: this.$t('quests.levels.2.title'),
+          code: 2,
+          desc: this.$t('quests.levels.2.desc'),
+          cost: '10',
+        },
+        {
+          level: this.$t('quests.levels.3.title'),
+          code: 3,
+          desc: this.$t('quests.levels.3.desc'),
+          cost: '70',
+        },
+        {
+          level: this.$t('quests.levels.4.title'),
+          code: 4,
+          desc: this.$t('quests.levels.4.desc'),
+          cost: '70',
+        },
+      ],
       specCount: 0,
       specIndex: {
         1: -1,
@@ -318,6 +613,33 @@ export default {
     this.SetLoader(false);
   },
   methods: {
+    selectRadio(idx) {
+      const radio = this.$refs.[`radio${idx}`];
+      for (let i = 0; i < Object.keys(this.$refs.[`radio${i}`]).length; i += 1) {
+        radio[i].checked = !radio[i].checked;
+      }
+    },
+    toFirstPeriod() {
+      this.period = 1;
+    },
+    toSecondPeriod() {
+      this.period = 2;
+    },
+    toThirdPeriod() {
+      this.period = 3;
+    },
+    toRiseViews() {
+      this.step += 1;
+    },
+    showPaymentModal() {
+      this.ShowModal({
+        key: modals.paymentOptions,
+        step: 1,
+      });
+    },
+    goBack() {
+      this.$router.go(-1);
+    },
     addSkillToBadge(event, object, index, key) {
       if (!this.selectedSkills[key].includes(object[index]) && this.selectedSkills[key].length <= 4) {
         this.selectedSkills[key].push(object[index]);
@@ -396,6 +718,186 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+.btn-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  margin: 20px 0 0 0;
+  &__left {
+    justify-content: flex-start;
+    margin: 35px 0 0 0;
+  }
+  &__btn {
+    width: 200px;
+    margin: 0 10px 0 0;
+    &_back {
+      width: 50px;
+    }
+    &:last-child {
+      margin: 0;
+    }
+  }
+}
+
+.cost__container {
+  display: flex;
+  align-self: center;
+}
+
+.card {
+  border: none;
+  &__level {
+    @include text-simple;
+    font-weight: 500;
+    font-size: 12px;
+    color: $white;
+    background: #F7CF00;
+    border-radius: 3px;
+    width: 120px;
+    padding: 2px 5px;
+    text-align: center;
+    &_reliable {
+      background: #BBC0C7;
+    }
+    &_checked {
+      background: #B79768;
+      width: 120px;
+    }
+  }
+  &__desc {
+    @include text-simple;
+    font-weight: 400;
+    font-size: 16px;
+    color: $black500;
+    margin-top: 12px;
+  }
+  &__cost {
+    @include text-simple;
+    color: $black800;
+    font-weight: 500;
+    font-size: 20px;
+    display: flex;
+    justify-content: center;
+  }
+}
+
+.level {
+  &__title {
+    @extend .period__title;
+  }
+  &__card {
+    background: $white;
+    border-radius: 6px;
+    padding: 20px 0;
+    display: grid;
+    grid-template-columns: 1fr 15fr 1fr;
+    margin: 20px 0 0 0;
+    transition: 0.5s;
+    &:hover {
+      cursor: pointer;
+      box-shadow: 0 0 10px 2px rgba(34, 60, 80, 0.09);
+    }
+    &_gold {
+      border: 1px solid #F7CF00;
+    }
+  }
+  &__option {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
+
+.radio {
+  &__input {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    border-radius: 50%;
+    width: 25px;
+    height: 25px;
+    border: 1px solid $blue;
+    cursor: pointer;
+    &:checked {
+      background: radial-gradient($blue 50%, rgba(255, 0, 0, 0) 55%);
+    }
+  }
+}
+
+.period {
+  &__choose {
+    @extend .period__title;
+    margin: 20px 0 0 0;
+  }
+  &__title {
+    @include text-simple;
+    font-weight: 400;
+    font-size: 16px;
+    color: $black800;
+    &_active {
+      color: $white;
+    }
+  }
+  &__container {
+    display: flex;
+    justify-content: space-between;
+    flex-direction: row;
+    max-width: 1180px;
+    width: 100%;
+    margin: 10px 0 20px 0;
+  }
+  &__period {
+    color: $black800;
+    background: $white;
+    padding: 12px 88px;
+    border-radius: 6px;
+    transition: .5s;
+    text-align: center;
+    width: inherit;
+    margin: 0 20px 0 0;
+    &:last-child {
+      margin: 0;
+    }
+    &:hover {
+      cursor: pointer;
+      box-shadow: 0 0 10px 2px rgba(34, 60, 80, 0.09);
+    }
+    &_active {
+      background: $blue;
+      color: $white;
+      &:hover {
+        cursor: pointer;
+        box-shadow: 0 0 10px 2px rgba(34, 60, 80, 0.09);
+      }
+    }
+    &:last-child {
+      margin: 0;
+    }
+    &__title {
+      color: $black800;
+      font-weight: 500;
+      font-size: 16px;
+    }
+  }
+}
+
+.main {
+  @include main;
+  &-white {
+    @include main;
+    background: $white;
+    background: #FFFFFF;
+    margin: 0 0 20px 0;
+    border-radius: 6px;
+    justify-content: center;
+  }
+  &__body {
+    max-width: 1180px;
+    width: 100%;
+    height: 100%;
+  }
+}
 
 .runtime {
   &__container {
@@ -621,6 +1123,13 @@ export default {
   &__title {
     @include text-simple;
     margin: 30px 0 0 0;
+  }
+  &__raising {
+    @include text-simple;
+    font-weight: 500;
+    font-size: 20px;
+    color: $black800;
+    margin: 0 0 20px 0;
   }
   &__page {
     font-weight: 500;
