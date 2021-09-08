cbd95a7b8021a9cc854cd8d35d3452822ccc2505
ghost-raven1 <sonofagronom@gmail.com>
2021-09-08 12:03:12 +0700

    hotfix/10131 fixed showImage modal

--- components/CtmModal/CtmModalShowImage/index.vue
+++ components/CtmModal/CtmModalShowImage/index.vue
@@ -3,24 +3,25 @@
     class="ctm-modal"
     :is-header="false"
   >
-    <div class="ctm-modal__content">
+    <div class="ctm-modal__content content">
       <img
+        class="content__img"
         :src="options.imageSrc"
         alt=""
       >
-      <div class="ctm-modal__desc desc">
-        <div
-          v-if="options.title"
-          class="desc__title"
-        >
-          {{ options.title }}
-        </div>
-        <div
-          v-if="options.desc"
-          class="desc__desc"
-        >
-          {{ options.desc }}
-        </div>
+    </div>
+    <div class="ctm-modal__desc desc">
+      <div
+        v-if="options.title"
+        class="desc__title"
+      >
+        {{ options.title }}
+      </div>
+      <div
+        v-if="options.desc"
+        class="desc__desc"
+      >
+        {{ options.desc }}
       </div>
     </div>
   </ctm-modal-box>
@@ -48,19 +49,32 @@ export default {
 .ctm-modal {
   @include modalKit;
   &__box {
-    max-width: 100% !important;
+    max-width: 80% !important;
+    max-height: 100% !important;
     width: auto !important;
     height: auto !important;
     padding: 0 !important;
   }
   &__content {
-    padding: 28px 28px 5px 28px !important;
+    padding: 0 28px 5px 28px !important;
   }
   &__desc {
     display: flex;
     flex-direction: row;
     justify-content: center;
     align-items: baseline;
+    padding: 28px 28px 5px 28px !important;
+  }
+}
+.content {
+  margin-top: 28px;
+  overflow-y: auto;
+  max-height: 800px;
+  height: 100%;
+  &__img {
+    width: 100%;
+    border-radius: 6px;
+    object-fit: cover;
   }
 }
 .desc {
@@ -69,15 +83,22 @@ export default {
   text-align: center;
   &__title {
     @include text-simple;
+    justify-content: center;
     margin: 10px 10px 0 0;
     font-size: 18px;
     font-weight: 500;
     display: flex;
+    overflow: hidden;
+    width: 100%;
   }
   &__desc {
+    @include text-simple;
+    display: flex;
     margin: 10px 10px 0 0;
     font-size: 16px;
     font-weight: 400;
+    overflow: hidden;
+    width: 100%;
   }
 }
 
