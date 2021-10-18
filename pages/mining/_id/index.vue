d16326e3d55f947f7ec927864f9d46b2490444b5
AlexBessmelcev <alexbessmelcev@gmail.com>
2021-10-18 21:11:40 +0700

    hotfix/14391V2 added modal foe incorrect reward

--- pages/mining/_id/index.vue
+++ pages/mining/_id/index.vue
@@ -559,9 +559,19 @@ export default {
     },
     async claimRewards() {
       this.SetLoader(true);
-      await this.connectToMetamask();
-      await this.$store.dispatch('web3/claimRewards', this.accountData.userPurse.address, this.fullRewardAmount);
-      await this.tokensDataUpdate();
+      if (this.fullRewardAmount > 0) {
+        await this.connectToMetamask();
+        await this.$store.dispatch('web3/claimRewards', this.accountData.userPurse.address, this.fullRewardAmount);
+        await this.tokensDataUpdate();
+      } else {
+        this.ShowModal({
+          key: modals.status,
+          img: require('~/assets/img/ui/warning.svg'),
+          title: this.$t('modals.transactionFail'),
+          recipient: '',
+          subtitle: this.$t('modals.nothingToClaim'),
+        });
+      }
       this.SetLoader(false);
     },
     async connectToMetamask() {
