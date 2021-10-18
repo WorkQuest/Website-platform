111b41e809d8a42c033aaf1b27164ff6525448a7
AlexBessmelcev <alexbessmelcev@gmail.com>
2021-10-18 15:21:22 +0700

    hotfix/14391 fix mining pages v5

--- pages/mining/_id/index.vue
+++ pages/mining/_id/index.vue
@@ -410,17 +410,18 @@ export default {
     if (this.$route.params.id === 'ETH') {
       await this.getWqtWethTokenDay();
       await this.getWqtWethTokenDayLast();
-      // await this.getWqtWethSwaps();
-      await this.tableWqtWethTokenDay();
     } else {
       await this.getWqtWbnbTokenDay();
       await this.getWqtWbnbTokenDayLast();
-      await this.tableWqtWbnbTokenDay();
-      // await this.getWqtWbnbSwaps();
     }
     await this.initTokenDays();
     await this.initGraphData();
     this.SetLoader(false);
+    if (this.$route.params.id === 'ETH') {
+      await this.tableWqtWethTokenDay();
+    } else {
+      await this.tableWqtWbnbTokenDay();
+    }
     await this.initTableData();
   },
 
