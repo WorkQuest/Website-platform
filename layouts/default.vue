8f9f553ea69c4042f2dd6ba399b361eb473f8f84
Elena <elena.kalichkina.rho@gmail.com>
2021-10-19 11:31:22 +0700

    feat/wiki turn on middlware

--- layouts/default.vue
+++ layouts/default.vue
@@ -791,7 +791,7 @@ import ClickOutside from 'vue-click-outside';
 export default {
   scrollToTop: true,
   name: 'DefaultLayout',
-  // middleware: 'auth',
+  middleware: 'auth',
   components: {},
   directives: {
     ClickOutside,
