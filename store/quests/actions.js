3292f374d47bda6168b3f9ff143a5d0c9772ade3
ghost-raven1 <sonofagronom@gmail.com>
2021-10-29 16:54:50 +0700

    feature/13525 added try catch in quests responses

--- store/quests/actions.js
+++ store/quests/actions.js
@@ -1,141 +1,273 @@
 export default {
   async questListForInvitation({ commit }, userId) {
-    const response = await this.$axios.$get(`/v1/employer/${userId}/quests`);
-    commit('setQuestListForInvitation', response.result);
-    return response.result;
+    try {
+      const response = await this.$axios.$get(`/v1/employer/${userId}/quests`);
+      commit('setQuestListForInvitation', response.result);
+      return response.result;
+    } catch (e) {
+      return console.log(e);
+    }
   },
   async workersList({ commit }) {
-    const response = await this.$axios.$get('/v1/profile/workers');
-    commit('setWorkersList', response.result);
-    return response.result;
+    try {
+      const response = await this.$axios.$get('/v1/profile/workers');
+      commit('setWorkersList', response.result);
+      return response.result;
+    } catch (e) {
+      return console.log(e);
+    }
   },
   async setCurrentWorker({ commit }, data) {
-    commit('setCurrentWorker', data);
-    return data;
+    try {
+      commit('setCurrentWorker', data);
+      return data;
+    } catch (e) {
+      return console.log(e);
+    }
   },
   async setInfoDataMode({ commit }, mode) {
-    commit('setInfoDataMode', mode);
+    try {
+      commit('setInfoDataMode', mode);
+    } catch (e) {
+      console.log(e);
+    }
   },
   async getCurrentStepCreateQuest({ commit }, data) {
-    commit('setCurrentStepCreateQuest', data);
+    try {
+      commit('setCurrentStepCreateQuest', data);
+    } catch (e) {
+      console.log(e);
+    }
   },
   async getCurrentStepEditQuest({ commit }, data) {
-    commit('setCurrentStepEditQuest', data);
+    try {
+      commit('setCurrentStepEditQuest', data);
+    } catch (e) {
+      console.log(e);
+    }
   },
   async questCreate({ commit }, payload) {
-    return await this.$axios.$post('/v1/quest/create', payload);
+    try {
+      const response = await this.$axios.$post('/v1/quest/create', payload);
+      return response.result;
+    } catch (e) {
+      return console.log(e);
+    }
   },
   async getAllQuests({ commit }, payload) {
-    const response = await this.$axios.$get(`/v1/quests?${payload || ''}`);
-    commit('setAllQuests', response.result);
-    return response.result;
+    try {
+      const response = await this.$axios.$get(`/v1/quests?${payload || ''}`);
+      commit('setAllQuests', response.result);
+      return response.result;
+    } catch (e) {
+      return console.log(e);
+    }
   },
   async getQuest({ commit }, payload) {
-    const response = await this.$axios.$get(`/v1/quest/${payload}`);
-    commit('setQuest', response.result);
-    return response.result;
+    try {
+      const response = await this.$axios.$get(`/v1/quest/${payload}`);
+      commit('setQuest', response.result);
+      return response.result;
+    } catch (e) {
+      return console.log(e);
+    }
   },
   async getUserQuests({ commit }, { userId, query = undefined }) {
-    const response = await this.$axios.$get(`/v1/employer/${userId}/quests?${query || ''}`);
-    commit('setUserQuests', response.result);
-    return response.result;
+    try {
+      const response = await this.$axios.$get(`/v1/employer/${userId}/quests?${query || ''}`);
+      commit('setUserQuests', response.result);
+      return response.result;
+    } catch (e) {
+      return console.log(e);
+    }
   },
   async getQuestsOnMap({ commit }, payload) {
-    const response = await this.$axios.$get(`/v1/quests/map/list-points?${payload}`);
-    commit('setAllQuests', response.result);
-    return response.result;
+    try {
+      const response = await this.$axios.$get(`/v1/quests/map/list-points?${payload}`);
+      commit('setAllQuests', response.result);
+      return response.result;
+    } catch (e) {
+      return console.log(e);
+    }
   },
   async getQuestsLocation({ commit }, payload) {
-    const response = await this.$axios.$get(`/v1/quests/map/points?${payload}`);
-    commit('setQuestsLocation', response.result);
-    return response.result;
+    try {
+      const response = await this.$axios.$get(`/v1/quests/map/points?${payload}`);
+      commit('setQuestsLocation', response.result);
+      return response.result;
+    } catch (e) {
+      return console.log(e);
+    }
   },
   async editQuest({ commit }, { payload, questId }) {
-    const response = await this.$axios.$put(`/v1/quest/${questId}`, payload);
-    commit('setQuestData', response.result);
-    return response.result;
+    try {
+      const response = await this.$axios.$put(`/v1/quest/${questId}`, payload);
+      commit('setQuestData', response.result);
+      return response.result;
+    } catch (e) {
+      return console.log(e);
+    }
   },
   async deleteQuest({ commit }, { questId }) {
-    const response = await this.$axios.$delete(`/v1/quest/${questId}`);
-    return response.result;
+    try {
+      const response = await this.$axios.$delete(`/v1/quest/${questId}`);
+      return response.result;
+    } catch (e) {
+      return console.log(e);
+    }
   },
   async startQuest({ commit }, { questId, data }) {
-    const response = await this.$axios.$post(`/v1/quest/${questId}/start`, data);
-    return response.result;
+    try {
+      const response = await this.$axios.$post(`/v1/quest/${questId}/start`, data);
+      return response.result;
+    } catch (e) {
+      return console.log(e);
+    }
   },
   setMapBounds({ commit }, payload) {
-    commit('setMapBounds', payload);
+    try {
+      commit('setMapBounds', payload);
+    } catch (e) {
+      console.log(e);
+    }
   },
   setMapCenter({ commit }, payload) {
-    commit('setMapCenter', payload);
+    try {
+      commit('setMapCenter', payload);
+    } catch (e) {
+      console.log(e);
+    }
   },
   async completeWorkOnQuest({ commit }, questId) {
-    const response = await this.$axios.$post(`/v1/quest/${questId}/complete-work`, questId);
-    return response.result; // Сдать квест на проверку Работодателю
+    try {
+      const response = await this.$axios.$post(`/v1/quest/${questId}/complete-work`, questId);
+      return response.result;
+    } catch (e) {
+      return console.log(e);
+    }
   },
   async rejectWorkOnQuest({ commit }, questId) {
-    const response = await this.$axios.$post(`/v1/quest/${questId}/reject-work`, questId);
-    return response.result;
+    try {
+      const response = await this.$axios.$post(`/v1/quest/${questId}/reject-work`, questId);
+      return response.result;
+    } catch (e) {
+      return console.log(e);
+    }
   },
   async acceptCompletedWorkOnQuest({ commit }, questId) {
-    const response = await this.$axios.$post(`/v1/quest/${questId}/accept-completed-work`, questId);
-    return response.result;
+    try {
+      const response = await this.$axios.$post(`/v1/quest/${questId}/accept-completed-work`, questId);
+      return response.result;
+    } catch (e) {
+      return console.log(e);
+    }
   },
   async acceptWorkOnQuest({ commit }, questId) {
-    const response = await this.$axios.$post(`/v1/quest/${questId}/accept-work`);
-    return response.result;
+    try {
+      const response = await this.$axios.$post(`/v1/quest/${questId}/accept-work`);
+      return response.result;
+    } catch (e) {
+      return console.log(e);
+    }
   },
   async rejectCompletedWorkOnQuest({ commit }, questId) {
-    const response = await this.$axios.$post(`/v1/quest/${questId}/reject-completed-work`, questId);
-    return response.result;
+    try {
+      const response = await this.$axios.$post(`/v1/quest/${questId}/reject-completed-work`, questId);
+      return response.result;
+    } catch (e) {
+      return console.log(e);
+    }
   },
   async closeQuest({ commit }, questId) {
-    const response = await this.$axios.$post(`/v1/quest/${questId}/close`, questId);
-    return response.result; // Закрыть квест status = 0, 5
+    try {
+      const response = await this.$axios.$post(`/v1/quest/${questId}/close`, questId);
+      return response.result;
+    } catch (e) {
+      return console.log(e);
+    }
   },
   async responsesToQuest({ commit }, questId) {
-    const response = await this.$axios.$get(`/v1/quest/${questId}/responses`);
-    commit('setResponses', response.result);
-    return response.result;
+    try {
+      const response = await this.$axios.$get(`/v1/quest/${questId}/responses`);
+      commit('setResponses', response.result);
+      return response.result;
+    } catch (e) {
+      return console.log(e);
+    }
   },
 
   // Testing
   // employer
-
   async inviteOnQuest({ commit }, payload, questId) {
-    return await this.$axios.$post(`/v1/quest/${questId}/invite`, payload);
+    try {
+      const response = await this.$axios.$post(`/v1/quest/${questId}/invite`, payload);
+      return response.result;
+    } catch (e) {
+      return console.log(e);
+    }
   },
 
   async respondOnQuest({ commit }, { data, questId }) {
-    const response = await this.$axios.$post(`/v1/quest/${questId}/response`, data);
-    return response.result;
+    try {
+      const response = await this.$axios.$post(`/v1/quest/${questId}/response`, data);
+      return response.result;
+      // TODO: Изменить запрос для бэка
+    } catch (e) {
+      return console.log(e);
+    }
   },
 
   // worker
   async setStarOnQuest({ commit }, id) {
-    return await this.$axios.$post(`/v1/quest/${id}/star`);
+    try {
+      const response = await this.$axios.$post(`/v1/quest/${id}/star`);
+      return response.result;
+    } catch (e) {
+      return console.log(e);
+    }
   },
   async takeAwayStarOnQuest(id) {
-    return await this.$axios.$delete(`/v1/quest/${id}/star`, id);
+    try {
+      const response = await this.$axios.$delete(`/v1/quest/${id}/star`, id);
+      return response.result;
+    } catch (e) {
+      return console.log(e);
+    }
   },
   async getStarredQuests({ commit }) {
-    const { data } = await this.$axios.$get('/v1/quests/starred');
-    commit('setStarredQuests', data.result);
-    return data.result;
-  }, // unused
+    try {
+      const { data } = await this.$axios.$get('/v1/quests/starred');
+      commit('setStarredQuests', data.result);
+      return data.result;
+    } catch (e) {
+      return console.log(e);
+    }
+  },
   async getResponsesToQuestForAuthUser({ commit }) {
-    const response = await this.$axios.$get('/v1/quest/responses/my');
-    commit('setResponsesMy', response.result);
-    return response.result;
+    try {
+      const response = await this.$axios.$get('/v1/quest/responses/my');
+      commit('setResponsesMy', response.result);
+      return response.result;
+    } catch (e) {
+      return console.log(e);
+    }
   },
 
   async acceptQuestInvitation({ commit }, responseId) {
-    const response = await this.$axios.$post(`/v1/quest/response/${responseId}/accept`);
-    return response.result;
+    try {
+      const response = await this.$axios.$post(`/v1/quest/response/${responseId}/accept`);
+      return response.result;
+    } catch (e) {
+      return console.log(e);
+    }
   }, // согласие на приглашение на квест
 
   async rejectQuestInvitation({ commit }, responseId) {
-    const response = await this.$axios.$post(`/v1/quest/employer/${responseId}/reject`);
-    return response.result;
+    try {
+      const response = await this.$axios.$post(`/v1/quest/employer/${responseId}/reject`);
+      return response.result;
+    } catch (e) {
+      return console.log(e);
+    }
   }, // отказ на приглашение на квест
 };
