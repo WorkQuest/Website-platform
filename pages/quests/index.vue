742526800045876a73e2cd8ed41bffae6942e987
JuSTeRFeeD <justerfeedy@gmail.com>
2021-11-19 17:04:41 +0700

    feature/16397/mainPageFIltersSearch merge fixes

--- pages/quests/index.vue
+++ pages/quests/index.vue
@@ -837,8 +837,9 @@ export default {
   }
   &__panel {
     display: grid;
-    grid-template-columns: repeat(5, 1fr);
+    grid-template-columns: repeat(6, 1fr);
     grid-gap: 10px;
+    width: 100%;
     span::before {
       padding-left: 10px;
       margin-right: 10px;
