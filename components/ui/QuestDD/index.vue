db2a8af46feb769fdc86e95e9322fc17c1c664f5
Zulwaran <zulwaran@gmail.com>
2021-12-22 19:06:20 +0700

    fix sharing menu z-index

--- components/ui/QuestDD/index.vue
+++ components/ui/QuestDD/index.vue
@@ -212,7 +212,6 @@ export default {
   box-shadow: 0 17px 17px rgba(0, 0, 0, 0.05), 0 5.125px 5.125px rgba(0, 0, 0, 0.03), 0 2.12866px 2.12866px rgba(0, 0, 0, 0.025), 0 0.769896px 0.769896px rgba(0, 0, 0, 0.0174206);
   border-radius: 6px;
   min-width: 120px;
-  z-index: 10000000;
   right: 2px;
   &__container {
     display: grid;
