92a8dbc78bb218a55135b3e80a30cab4f6632a71
Zulwaran <zulwaran@gmail.com>
2021-12-27 10:20:35 +0700

    update skills

--- components/app/pages/settings/Skills.vue
+++ components/app/pages/settings/Skills.vue
@@ -4,7 +4,7 @@
       v-if="true"
       class="skills__title"
     >
-      {{ $t('settings.employmentInfo') }}
+      {{ $t("settings.employmentInfo") }}
     </div>
     <div
       v-if="true"
@@ -18,7 +18,7 @@
         <div class="skills__add-info">
           <base-dd
             v-model="priorityIndex"
-            class="specialization__dd"
+            class="skills__drop-down"
             type="gray"
             :placeholder="$t('priority.title')"
             :items="priority"
@@ -27,7 +27,7 @@
           />
           <base-dd
             v-model="distantIndex"
-            class="specialization__dd"
+            class="skills__drop-down"
             type="gray"
             :placeholder="$t('settings.distantWork.select')"
             :items="distantWork"
@@ -36,18 +36,16 @@
           />
           <base-field
             v-model="perHour"
-            class="specialization__skills"
-            :placeholder="perHour ||$t('priority.title')"
+            class="skills__cost"
+            :placeholder="perHour || $t('priority.title')"
             :label="$t('settings.costPerHour')"
             :name="$t('settings.costPerHour')"
             type="gray"
           />
         </div>
-        <div class="page__btn">
-          <base-btn
-            class="btn__save"
-          >
-            {{ $t('settings.save') }}
+        <div class="skills__save">
+          <base-btn class="skills__btn">
+            {{ $t("settings.save") }}
           </base-btn>
         </div>
       </div>
@@ -58,50 +56,42 @@
 <script>
 export default {
   name: 'Skills',
-
+  data() {
+    return {
+      perHour: 0,
+      priorityIndex: -1,
+      distantIndex: -1,
+    };
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 .skills {
-&__title {
-font-family: Inter, Arial, sans-serif;
-font-weight: 500;
-font-size: 25px;
-line-height: 32px;
-}
-&__container{
+  display: grid;
+  gap: 20px;
+  &__title {
+    font-family: Inter, Arial, sans-serif;
+    font-weight: 500;
+    font-size: 25px;
+    line-height: 32px;
+  }
+  &__container {
     background: $white;
-}
+    padding: 20px;
+  }
   &__add-info {
-    display: flex;
-    flex-wrap: nowrap;
-    grid-gap: 20px;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    gap: 20px;
     margin-top: 20px;
   }
-}
-/* .skills {
-
+  &__save {
+    display: flex;
+    justify-content: flex-end;
+  }
   &__btn {
-    &-add {
-      text-align: center;
-      margin-top: 20px;
-      width: 250px;
-      background: #FFFFFF;
-      color: #0083C7;
-      border: 1px solid #bce8ff;
-      &_disabled {
-        color: #aaaaaa !important;
-        border: 0.5px solid #aaaaaa;
-      }
-    }
-    &-add:hover {
-      background: #bce8ff;
-    }
+    max-width: 250px;
   }
-  &__error {
-    color: #f36262;
-    margin-bottom: 10px;
-  }
-} */
+}
 </style>
