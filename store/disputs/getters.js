a5a9a5076a7106b30fc2fa885b42a2bcd386373d
AlexBessmelcev <alexbessmelcev@gmail.com>
2022-01-13 18:33:08 +0700

    feature/15774/disputes add small logic

---
+++ store/disputs/getters.js
@@ -0,0 +1,4 @@
+export default {
+  getDisput: (state) => state.disput,
+  getUserDisputs: (state) => state.userDisputs,
+};
---
+++ store/disputs/mutations.js
@@ -0,0 +1,8 @@
+export default {
+  setDisput(state, data) {
+    state.disput = data;
+  },
+  setUserDisputs(state, data) {
+    state.userDisputs = data;
+  },
+};
