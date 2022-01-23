688ffe92a50cbcf2c85d9a0b986ab52adf97272d
matykoks <matyko.ks@gmail.com>
2022-01-24 02:09:14 +0700

    added component search with map, done first part of refactoring, component report checkbox status

--- pages/workers/index.vue
+++ pages/workers/index.vue
@@ -4,92 +4,10 @@
       class="employees__map-block"
       :is-show-map="isShowMap"
     />
-    <div
-      class="employees__search search"
-      :class="{ 'search_map-hidden': !isShowMap }"
-    >
-      <div class="search__block">
-        <div class="search__content">
-          <div class="search__toggle">
-            <base-checkbox
-              v-model="isShowMap"
-              data-selector="ACTION-IS-SHOW-MAP-USER"
-              name="map"
-              :label="$t('quests.ui.showMap')"
-            />
-          </div>
-          <div
-            class="search__inputs"
-            data-selector="ACTION-TOGGLE-SEARCH-DD-USER"
-            @click="toggleSearchDD"
-          >
-            <base-field
-              v-model="search"
-              v-click-outside="hideSearchDD"
-              data-selector="INPUT-SEARCH-ADDRESS-USER"
-              class="search__input"
-              is-search
-              :placeholder="$t('quests.ui.search')"
-              mode="icon"
-              :selector="isSearchDDStatus"
-              @selector="getAddressInfo(search)"
-            >
-              <template v-slot:left />
-              <template v-slot:selector>
-                <div
-                  v-if="addresses.length"
-                  class="selector"
-                  :class="{'selector_hide': isSearchDDStatus === false}"
-                >
-                  <div class="selector__items">
-                    <div
-                      v-for="(item, i) in addresses"
-                      :key="i"
-                      :data-selector="`ACTION-SELECT-ADDRESS-${item.key}`"
-                      class="selector__item"
-                      @click="selectAddress(item)"
-                    >
-                      {{ item.formatted }}
-                    </div>
-                  </div>
-                </div>
-              </template>
-            </base-field>
-          </div>
-          <div class="search__dd">
-            <base-dd
-              v-model="distanceIndex"
-              :items="distanceItems"
-            />
-          </div>
-          <div class="search__actions">
-            <base-btn
-              class="search__btn"
-              data-selector="ACTION-MAP-CENTER-CHANGE-USER"
-              @click="centerChange"
-            >
-              {{ $t('workers.searchWorkers') }}
-            </base-btn>
-          </div>
-        </div>
-      </div>
-      <div class="search__filter filter">
-        <div class="filter__dd">
-          <base-dd
-            v-model="distanceIndex"
-            :items="distanceItems"
-          />
-        </div>
-        <div class="filter__toggle">
-          <base-checkbox
-            v-model="isShowMap"
-            name="map"
-            data-selector="ACTION-IS-SHOW-MAP-USER"
-            :label="$t('quests.ui.showMap')"
-          />
-        </div>
-      </div>
-    </div>
+    <search-block
+      class="employees__search"
+      @isShowMap="isShowMap = $event"
+    />
     <div class="employees__content">
       <h2 class="employees__title">
         {{ $t('workers.topWorkers') }}
@@ -131,15 +49,9 @@
 
 <script>
 import { mapGetters } from 'vuex';
-import { GeoCode } from 'geo-coder';
-import ClickOutside from 'vue-click-outside';
-import modals from '~/store/modals/modals';
 
 export default {
   name: 'Employees',
-  directives: {
-    ClickOutside,
-  },
   data() {
     return {
       page: 1,
@@ -152,12 +64,6 @@ export default {
       isShowMap: true,
       isFetching: false,
       boundsTimeout: null,
-
-      isSearchDDStatus: true,
-      search: '',
-      distanceIndex: 0,
-      addresses: [],
-      coordinates: null,
     };
   },
   computed: {
@@ -167,14 +73,6 @@ export default {
       employeeCount: 'quests/getEmployeeCount',
     }),
     totalPages() { return Math.ceil(this.employeeCount / this.query.limit); },
-
-    distanceItems() {
-      return [
-        this.$t('quests.distance.100'),
-        this.$t('quests.distance.500'),
-        this.$t('quests.distance.1000'),
-      ];
-    },
   },
   watch: {
     async isShowMap(newVal) {
@@ -270,40 +168,9 @@ export default {
       await this.fetchEmployeeList(true);
     },
 
-    toggleSearchDD() {
-      this.isSearchDDStatus = !this.isSearchDDStatus;
-    },
-    hideSearchDD() {
-      this.isSearchDDStatus = false;
-    },
-    showPriceSearch() {
-      this.ShowModal({
-        key: modals.priceSearch,
-      });
-    },
     showDetails(worker) {
       this.$store.dispatch('quests/setCurrentWorker', worker);
       this.$router.push(`/profile/${worker.id}`);
-    },
-    centerChange() {
-      this.$store.dispatch('quests/setMapCenter', this.coordinates);
-    },
-    selectAddress(address) {
-      this.search = address.formatted;
-      this.addresses = [];
-    },
-    async getAddressInfo(address) {
-      let response = [];
-      const geoCode = new GeoCode('google', { key: process.env.GMAPKEY });
-      try {
-        if (address.length) {
-          response = await geoCode.geolookup(address);
-          this.addresses = JSON.parse(JSON.stringify(response));
-          this.coordinates = JSON.parse(JSON.stringify({ lng: response[0].lng, lat: response[0].lat }));
-        }
-      } catch (e) {
-        console.log(e);
-      }
     },
   },
 };
@@ -314,10 +181,15 @@ export default {
 .employees {
   @include text-simple;
 
-  &__content {
+  &__search {
     max-width: 1180px;
+    margin: 30px auto;
+  }
+
+  &__content {
     width: 100%;
     height: 100%;
+    max-width: 1180px;
     margin: 0 auto;
   }
 
@@ -345,145 +217,12 @@ export default {
   }
 }
 
-.selector {
-  @include box;
-  width: 100%;
-  z-index: 140;
-  &_hide {
-    display: none;
-  }
-  &__items {
-    background: #FFFFFF;
-    display: grid;
-    grid-template-columns: 1fr;
-    width: 100%;
-  }
-  &__item {
-    @include text-simple;
-    padding: 15px 20px;
-    background: #FFFFFF;
-    font-weight: 500;
-    font-size: 16px;
-    color: $black800;
-    cursor: pointer;
-    transition: .3s;
-    &:hover {
-      background: #F3F7FA;
-    }
-  }
-}
-
 .map {
   &__container {
     position: relative;
   }
 }
 
-.checkbox {
-  &-input {
-    width: 24px;
-    height: 24px;
-  }
-  &__label {
-    display: flex;
-    flex-direction: row;
-    flex-shrink: 0;
-    margin: 0 0 0 5px;
-    font-size: 16px;
-  }
-}
-
-.search {
-  height: 0;
-  &__block {
-    position: relative;
-    max-width: 1180px;
-    height: 83px;
-    bottom: 100px;
-    left: 0;
-    right: 0;
-    margin: auto;
-    z-index: 1200;
-    @include box;
-  }
-  &__content {
-    display: grid;
-    grid-template-columns: 154px 1fr 143px 260px;
-    align-items: center;
-    height: 100%;
-    justify-items: center;
-  }
-  &__dd {
-    display: flex;
-    border-left: 1px solid #F7F8FA;
-    justify-items: center;
-    align-items: center;
-    height: 100%;
-    width: 146px;
-  }
-  &__icon {
-    margin-bottom: -10px;
-    &::before {
-      font-size: 24px;
-      color: $blue;
-    }
-  }
-  &__inputs {
-    padding: 0 20px;
-    width: 100%;
-    display: grid;
-    align-items: center;
-  }
-  &__input {
-    display: flex;
-    align-items: center;
-  }
-  &__btn {
-    max-width: 220px;
-  }
-  &__toggle {
-    height: 100%;
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border-right: 1px solid #F7F8FA;
-  }
-  &__actions {
-    height: 100%;
-    border-left: 1px solid #F7F8FA;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 100%;
-  }
-  &__filter {
-    display: none;
-  }
-  &_map-hidden {
-    padding: 20px 0;
-    height: auto;
-    .search__block {
-      bottom: 0;
-    }
-  }
-}
-
-@include _1300 {
-  .search {
-    &__content {
-      grid-template-columns: repeat(4, auto);
-    }
-
-    &__actions, &__toggle {
-      padding: 10px;
-    }
-
-    &__block {
-      width: 80%;
-    }
-  }
-}
 @include _1199 {
   .employees {
 
@@ -494,64 +233,17 @@ export default {
     &__cards {
       grid-template-columns: repeat(3, 1fr);
     }
-  }
 
+  }
 }
+
 @include _991 {
   .employees {
 
     &__cards {
       grid-template-columns: repeat(2, 1fr);
     }
-  }
 
-  .dd {
-    grid-column: 1/3;
-  }
-  .search {
-    width: 100%;
-    position: initial;
-    padding: 20px;
-    height: auto;
-    &__block {
-      width: 100%;
-      position: initial;
-    }
-    &__content {
-      grid-template-columns: 1fr 0.5fr;
-      padding: 0 10px;
-      grid-gap: 10px;
-    }
-    &__toggle, &__dd {
-      display: none;
-    }
-    &__actions {
-      border: none;
-    }
-    &__inputs {
-      padding: 0 10px;
-    }
-    &__filter {
-      display: flex;
-      flex-direction: row;
-      position: relative;
-      justify-content: space-between;
-      top: 20px;
-      align-content: center;
-    }
-  }
-  .dd__btn {
-    justify-content: center;
-    padding: 0 0;
-  }
-  .filter {
-    &__toggle {
-      text-align: center;
-      display: flex;
-      padding: 10px;
-      border-radius: 6px;
-      background: #FFFFFF;
-    }
   }
 }
 
@@ -561,7 +253,7 @@ export default {
     &__cards {
       grid-template-columns: 1fr;
     }
-  }
 
+  }
 }
 </style>
