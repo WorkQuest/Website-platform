43e73c96ccad958c1e1b72c08e1d8e6ceb368970
ghost1 <sonofagronom@gmail.com>
2022-01-26 12:44:35 +0700

    fix/20577/quests-page-refactoring added refactored quests page, action getAllQuests, questCard component

--- plugins/injectComponents.js
+++ plugins/injectComponents.js
@@ -25,6 +25,8 @@ import FilesUploader from '~/components/ui/FilesUploader';
 import FilesPreview from '~/components/ui/FilesPreview';
 import EmptyData from '~/components/app/info/emptyData';
 import EmployeeCard from '~/components/app/pages/common/employeeCard';
+import QuestCards from '~/components/app/pages/common/quests';
+import QuestCard from '~/components/app/pages/common/questCard';
 import FiltersPanel from '~/components/app/panels/filters';
 import SearchWithMap from '~/components/app/SearchWithMap';
 
@@ -55,3 +57,5 @@ Vue.component('empty-data', EmptyData);
 Vue.component('employee-card', EmployeeCard);
 Vue.component('filters-panel', FiltersPanel);
 Vue.component('search-with-map', SearchWithMap);
+Vue.component('quest-cards', QuestCards);
+Vue.component('quest-card', QuestCard);
