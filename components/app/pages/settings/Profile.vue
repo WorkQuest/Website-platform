fd3ed6739f6cb659861e6ccf24ae4962418f3730
ghost1 <sonofagronom@gmail.com>
2022-02-03 15:51:12 +0700

    fix/21465/fix-main-phone-number fixed textarea styles

--- components/app/pages/settings/Profile.vue
+++ components/app/pages/settings/Profile.vue
@@ -596,6 +596,12 @@ export default {
     display: grid;
     grid-column-start: 1;
     grid-column-end: 3;
+    background-color: $black0;
+    resize: none;
+    padding: 10px 10px 0 10px;
+    margin: 0;
+    border-radius: 6px;
+    border: 1px solid transparent;
     &::placeholder {
       color: $black200;
     }
