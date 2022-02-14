68bd6b08398776419d9d9ce014b2db7230cd92b4
ghost1 <sonofagronom@gmail.com>
2022-02-14 14:00:30 +0700

    fix/21982/fix-styles-star-rating changed structure starRating

--- components/app/pages/quests_id/starRating.vue
+++ components/app/pages/quests_id/starRating.vue
@@ -1,26 +1,66 @@
 <template>
-  <ul class="rating-area">
-    <li
-      v-for="(star,index) in starsNumber"
-      :key="index"
-      class="rating-area__star"
-      :class="getStarStyles"
-    >
+  <!--  <ul class="rating-area">-->
+  <!--    <li-->
+  <!--      v-for="(star,index) in starsNumber"-->
+  <!--      :key="index"-->
+  <!--      class="rating-area__star"-->
+  <!--      :class="getStarStyles"-->
+  <!--    >-->
+  <!--      <input-->
+  <!--        :id="`${ratingType}-star-${questIndex}-${starsNumber-index}`"-->
+  <!--        type="radio"-->
+  <!--        name="rating"-->
+  <!--        :checked="(starsNumber-index)===rating"-->
+  <!--        :aria-label="`«${starsNumber-index}»`"-->
+  <!--        @click="$emit('input', starsNumber-index)"-->
+  <!--      >-->
+  <!--      <label-->
+  <!--        :for="`${ratingType}-star-${questIndex}-${starsNumber-index}`"-->
+  <!--        :title="`«${starsNumber-index}»`"-->
+  <!--        class="rating-area__star-label"-->
+  <!--        :class="fillGoldenStars(index)"-->
+  <!--      />-->
+  <!--    </li>-->
+  <!--  </ul>-->
+  <fieldset class="rating">
+    <div class="rating__group">
       <input
-        :id="`${ratingType}-star-${questIndex}-${starsNumber-index}`"
         type="radio"
         name="rating"
-        :checked="(starsNumber-index)===rating"
-        @click="$emit('input', starsNumber-index)"
+        class="rating__star"
+        value="1"
+        aria-label="1"
       >
-      <label
-        :for="`${ratingType}-star-${questIndex}-${starsNumber-index}`"
-        :title="`«${starsNumber-index}»`"
-        class="rating-area__star-label"
-        :class="fillGoldenStars(index)"
-      />
-    </li>
-  </ul>
+      <input
+        type="radio"
+        name="rating"
+        class="rating__star"
+        value="2"
+        aria-label="2"
+      >
+      <input
+        type="radio"
+        name="rating"
+        class="rating__star"
+        value="3"
+        aria-label="3"
+      >
+      <input
+        type="radio"
+        name="rating"
+        class="rating__star"
+        value="4"
+        aria-label="4"
+      >
+      <input
+        type="radio"
+        name="rating"
+        class="rating__star"
+        value="5"
+        aria-label="5"
+      >
+    </div>
+  </fieldset>
 </template>
 <script>
 export default {
@@ -63,44 +103,110 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.rating-area {
-  overflow: hidden;
-  display: flex;
-  flex-direction: row-reverse;
-  justify-content: flex-end;
-  height: 24px;
-  &__star > input {
-    display: none;
+.rating {
+  margin: 0;
+  padding: 0;
+  border: none;
+  &__group {
+    position: relative;
+    width: 7.5em;
+    height: 1.5em;
+    background-image: url('assets/img/ui/star-empty.svg');
+    background-size: 1.5em auto;
+    background-repeat: repeat-x;
   }
-  &__star-label {
-    float: right;
-    padding: 0;
-    cursor: pointer;
-    font-size: 25px;
-    line-height: 25px;
-    color: lightgrey;
-    text-shadow: 1px 1px #bbb;
-    &_golden {
-      color: gold;
+  &__star {
+    position: absolute;
+    margin: 0;
+    top: 0;
+    left: 0;
+    width: 1.5em;
+    height: 1.5em;
+    background-size: 1.5em auto;
+    background-repeat: repeat-x;
+    font-size: inherit;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    &:nth-of-type(1) {
+      z-index: 5;
+      width: 1.5em;
+    }
+    &:nth-of-type(2) {
+      z-index: 4;
+      width: 3em;
+    }
+    &:nth-of-type(3) {
+      z-index: 3;
+      width: 4.5em;
+    }
+    &:nth-of-type(4) {
+      z-index: 2;
+      width: 6em;
+    }
+    &:nth-of-type(5) {
+      z-index: 1;
+      width: 7.5em;
+    }
+    &:focus {
+      outline: none;
+    }
+    &:focus ~ .rating__focus {
+      position: absolute;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      z-index: -1;
+      outline: solid 0.2em $blue;
+      outline-offset: 0.2em;
+    }
+    &:checked, &:hover {
+      background-image: url('assets/img/ui/star.svg');
+    }
+    &:hover ~ .rating__star {
+      background-image: url('assets/img/ui/star-empty.svg');
     }
   }
-  &__star > label:before {
-    content: '★';
-  }
-  &__star:hover > label {
-    color: gold;
-  }
-  &__star:hover ~ &__star > label {
-    color: gold;
-    text-shadow: 1px 1px goldenrod;
-  }
 }
-.disabled {
-  pointer-events: none;
-}
-@include _575 {
-  .rating-area__star-label {
-      font-size: 20px;
-    }
-}
+//.rating-area {
+//  overflow: hidden;
+//  display: flex;
+//  flex-direction: row-reverse;
+//  justify-content: flex-end;
+//  height: 24px;
+//  &__star > input {
+//    display: none;
+//  }
+//  &__star-label {
+//    float: right;
+//    padding: 0;
+//    cursor: pointer;
+//    font-size: 25px;
+//    line-height: 25px;
+//    color: lightgrey;
+//    text-shadow: 1px 1px #bbb;
+//    &_golden {
+//      color: gold;
+//    }
+//  }
+//  &__star > label:before {
+//    content: '★';
+//  }
+//  &__star:hover > label {
+//    color: gold;
+//  }
+//  &__star:hover ~ &__star > label {
+//    color: gold;
+//    text-shadow: 1px 1px goldenrod;
+//  }
+//}
+//.disabled {
+//  pointer-events: none;
+//}
+//@include _575 {
+//  .rating-area__star-label {
+//      font-size: 20px;
+//    }
+//}
 </style>
