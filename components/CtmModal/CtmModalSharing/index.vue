ee5172591df7eb24e0e6dc827d4421874f078a38
matykoks <matyko.ks@gmail.com>
2022-02-16 00:57:47 +0700

    refactored modal

--- components/CtmModal/CtmModalSharing/index.vue
+++ components/CtmModal/CtmModalSharing/index.vue
@@ -1,44 +1,41 @@
 <template>
-  <ctm-modal-box
-    class="messageSend"
-    :title="$t('sharing.title')"
-  >
-    <div class="ctm-modal__content">
-      <span>{{ $t('sharing.socialText') }}</span>
-      <div class="social__icons">
-        <button class="social__btn social__btn_telegram">
+  <ctm-modal-box :title="$t('sharing.title')">
+    <div class="ctm-modal__content share">
+      <span class="share__text">{{ $t('sharing.socialText') }}</span>
+      <div class="share__socials">
+        <button
+          v-for="(link,idx) in socialLinks"
+          :key="idx"
+          class="socials__btn"
+          :class="`socials__btn-${link.icon}`"
+        >
           <img
-            class="social__btn_telegram"
+            v-if="link.icon === 'telegram'"
+            class="socials__btn-telegram"
             alt=""
             src="~assets/img/icons/telegram.png"
           >
-        </button>
-        <button class="social__btn social__btn_twitter">
-          <span class="icon-twitter" />
-        </button>
-        <button class="social__btn social__btn_facebook">
-          <span class="icon-facebook" />
-        </button>
-        <button class="social__btn social__btn_LinkedIn">
-          <span class="icon-LinkedIn" />
+          <span
+            v-else
+            class="socials__icon"
+            :class="`icon-${link.icon}`"
+          />
         </button>
       </div>
-      <span class="content__text">{{ $t('sharing.share') }}</span>
-      <div class="flex__two-cols">
-        <div class="code__container">
-          <span class="code__text">{{ sharingLink }}</span>
+      <span class="share__text">{{ $t('sharing.share') }}</span>
+      <div class="share__link">
+        <div class="share__text share__text_hidden">
+          {{ sharingLink }}
         </div>
-        <div>
-          <button
-            v-clipboard:copy="sharingLink"
-            v-clipboard:success="ClipboardSuccessHandler"
-            v-clipboard:error="ClipboardErrorHandler"
-            class="btn__copy"
-            type="button"
-          >
-            <span class="icon-copy" />
-          </button>
-        </div>
+        <button
+          v-clipboard:copy="sharingLink"
+          v-clipboard:success="ClipboardSuccessHandler"
+          v-clipboard:error="ClipboardErrorHandler"
+          class="share__copy"
+          type="button"
+        >
+          <span class="icon-copy" />
+        </button>
       </div>
     </div>
   </ctm-modal-box>
@@ -47,331 +44,101 @@
 <script>
 import { mapGetters } from 'vuex';
 import { Path } from '~/utils/enums';
-import modals from '~/store/modals/modals';
 
 export default {
-  name: 'ModalOpenADeposit',
+  name: 'ModalSharing',
   computed: {
     ...mapGetters({
       options: 'modals/getOptions',
     }),
+    socialLinks() {
+      return [
+        { icon: 'telegram', url: '' },
+        { icon: 'twitter', url: '' },
+        { icon: 'facebook', url: '' },
+        { icon: 'LinkedIn', url: '' },
+      ];
+    },
     sharingLink() {
-      if (this.options.mode === 'quest') return `${window.location.origin}${Path.QUESTS}/${this.options.itemId}`;
-      if (this.options.mode === 'profile') return `${window.location.origin}${Path.PROFILE}/${this.options.itemId}`;
+      const { itemId } = this.options;
+      const url = window.location.origin;
+      if (this.options.mode === 'quest') return `${url}${Path.QUESTS}/${itemId}`;
+      if (this.options.mode === 'profile') return `${url}${Path.PROFILE}/${itemId}`;
       return 'error';
-    },
-  },
-  methods: {
-    hide() {
-      this.CloseModal();
-    },
-    showDepositIsOpenedModal() {
-      this.ShowModal({
-        key: modals.status,
-        img: require('~/assets/img/ui/transactionSend.svg'),
-        title: this.$t('saving.depositIsOpened'),
-        subTitle: '',
-        path: '/savings/1',
-      });
     },
   },
 };
 </script>
 
 <style lang="scss" scoped>
-.ctm-modal {
-  @include modalKit;
-  &__content {
-    margin: 25px 0 0 0;
-  }
-  &__content-btns {
-    .btn-group {
-      display: grid;
-      grid-template-columns: repeat(2, calc(50% - 10px));
-      grid-gap: 20px;
-      gap: 20px;
-      margin-top: 25px;
-      .btn {
-        box-sizing: border-box;
-        font-weight: 400;
-        font-size: 16px;
-        color: #0083C7;
-        border: 1px solid #0083C71A;
-        border-radius: 6px;
-        transition: .3s;
-        background-color: #fff;
-        &:hover {
-          background-color: #0083C71A;
-          border: 0;
-        }
-        &_bl {
-          @extend .btn;
-          background-color: #0083C7;
-          border: unset;
-          color: #fff;
-          &:hover {
-            background-color: #103d7c;
-          }
-        }
-      }
+.share {
+  min-width: 382px !important;
+  &__text {
+    @include text-simple;
+    @include normal-font-size;
+    &_hidden {
+      overflow: hidden;
+      white-space: nowrap;
+      text-overflow: ellipsis;
     }
   }
-  &__label {
-    margin-bottom: 5px;
+  &__socials {
+    display: grid;
+    grid-template-columns: repeat(4, 40px);
+    grid-template-rows: 40px;
+    grid-gap: 25px;
+    margin: 10px 0 25px 0;
   }
-  &__content {
-    padding-top: 0 !important;
+  &__link {
+    display: flex;
+    margin-top: 5px;
+    padding: 12.5px 54px 12.5px 15px;
+    border: 1px solid $black0;
+    border-radius: 6px;
+  }
+  &__copy {
+    position: absolute;
+    right: 45px;
+    & span {
+      font-size: 20px;
+      color: $blue;
+    }
   }
 }
-.social {
-  &__icons {
-    display: grid;
-    grid-template-columns: repeat(5, 40px);
-    grid-gap: 30px;
-    margin-bottom: 25px;
-  }
+.socials {
   &__btn {
-    transition: .5s;
-    width: 40px;
-    height: 40px;
-    background: #F7F8FA;
-    margin-top: 10px;
-    border-radius: 6px;
     display: flex;
     align-items: center;
     justify-content: center;
+    border-radius: 6px;
+    background: $black0;
     &:hover {
-      background: #0083C7;
-    }
-    &:hover {
-      span:before {
-        background: white;
-        -webkit-background-clip: text;
-        -webkit-text-fill-color: transparent;
+      background: $blue;
+      & span {
+        color: $white;
       }
-      img {
+      & img {
         transition: 0.5s;
         filter: brightness(1000%) grayscale(100%);
       }
     }
-    &_telegram {
-      img {
-        height: 14px;
-        width: 16px;
-      }
-    }
-    &_instagram {
-      span:before {
-        font-size: 18px;
-        background: linear-gradient(180deg, #C540F3 0%, #FF8C05 100%);
-        -webkit-background-clip: text;
-        -webkit-text-fill-color: transparent;
-      }
-    }
-    &_twitter {
-      span:before {
-        font-size: 18px;
-        color: #24CAFF;
-      }
-    }
-    &_facebook {
-      span:before {
-        font-size: 18px;
-        color: #3B67D7;
-      }
-    }
-    &_LinkedIn {
-      span:before {
-        font-size: 18px;
-        color: #0A7EEA;
-      }
-    }
-    &_workQuest {
-      img {
-        width: 60%;
-      }
-    }
   }
-}
-.messageSend {
-  max-width: 495px !important;
-  &__content {
-    display: grid;
-    grid-template-columns: 1fr;
-    justify-items: center;
-    grid-gap: 20px;
-  }
-  &__action {
-    margin-top: 10px;
-  }
-}
-.flex {
-  &__two-cols {
-    display: flex;
-    justify-content: space-between;
-    align-items: flex-end;
-  }
-}
-.content {
-  &__text {
-    @include text-simple;
-    margin: 20px 0;
-    font-weight: 400;
-    color: $black700;
-    font-size: 18px;
-    &_grey {
-      @extend .content__text;
-      font-size: 16px;
-      color: $black400;
-    }
-  }
-}
-.icon {
-  &-copy:before {
-    content: "\e996";
-    color: $blue;
+  &__icon {
     font-size: 20px;
   }
-}
-.qr {
-  &__container {
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    margin: 20px 0;
+  &__btn-telegram img {
+    width: 16px;
+    height: 14px;
+  }
+  &__btn-twitter span {
+    color: #24CAFF;
+  }
+  &__btn-facebook span {
+    color: #3B67D7;
+  }
+  &__btn-LinkedIn span {
+    color: #0A7EEA;
   }
 }
-.code {
-  &__container {
-    display: flex;
-    border: 1px solid $black0;
-    border-radius: 6px;
-    justify-content: space-between;
-    padding: 12px;
-    margin: 5px 10px 0 0;
-    width: 100%;
-  }
-  &__text {
-    font-weight: 400;
-    font-size: 16px;
-    color: $black800;
-  }
-}
-.hide {
-  display: none;
-}
-.grid {
-  &__2col {
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    justify-content: space-between;
-    align-items: flex-end;
-    grid-gap: 10px;
-  }
-  &__3col {
-    display: grid;
-    grid-template-columns: 6fr 1fr 6fr;
-    justify-content: space-between;
-    align-items: flex-end;
-  }
-}
-.step {
-  &__number {
-    padding: 10px;
-  }
-  &__container {
-    &_grid {
-      display: grid;
-      grid-template-columns: repeat(2, 1fr);
-    }
-  }
-}
-.step-panel {
-  display: flex;
-  flex-direction: row;
-  align-items: flex-start;
-  &__step {
-    @include text-simple;
-    background: rgba(0, 131, 199, 0.1);
-    border-radius: 6px;
-    font-weight: 400;
-    font-size: 16px;
-    color: $black500;
-    padding: 10px;
-    &_active {
-      @extend .step-panel__step;
-      color: $white;
-      background: $blue;
-    }
-  }
-}
-.line {
-  display: block;
-  height: 1px;
-  border-top: 1px solid rgba(0, 131, 199, 0.1);
-  margin: auto 0;
-  padding: 0;
-  width: 33px;
-  &__active {
-    @extend .line;
-    border-top: 1px solid $blue;
-  }
-}
-.ctm-modal {
-  &__content-field {
-    margin: 15px 0 0 0;
-  }
-  &__equal {
-    margin: 0 0 35px 10px;
-  }
-}
-.ctm-modal {
-  @include modalKit;
-}
-.input {
-  &_white {
-    border-radius: 6px;
-    border: 1px solid $black0;
-    padding: 11px 20px 11px 15px;
-    height: 46px;
-    width: 100%;
-    background-color: $white;
-    resize: none;
-    &::placeholder {
-      color: $black800;
-    }
-  }
-}
-.btn {
-  &__container {
-    display: flex;
-    flex-direction: row-reverse;
-    justify-content: space-between;
-    margin: 15px 0 0 0;
-  }
-  &__wrapper {
-    width: 45%;
-  }
-  &__onebtn {
-    width: 100%;
-  }
-  &__copy {
-    background: $white;
-    border: 1px solid $black0;
-    padding: 11px;
-    border-radius: 6px;
-  }
-}
-.messageSend {
-  max-width: 430px !important;
-  &__content {
-    display: grid;
-    grid-template-columns: 1fr;
-    justify-items: center;
-    grid-gap: 20px;
-  }
-  &__action {
-    margin-top: 10px;
-  }
-}
+
 </style>
