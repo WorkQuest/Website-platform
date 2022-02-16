58683b147a6ce99649776a3e79e97fdeccb11891
ghost1 <sonofagronom@gmail.com>
2022-02-16 14:32:09 +0700

    feat/21982/fix-styles-star-rating deleted useless code

--- components/app/CardQuest.vue
+++ components/app/CardQuest.vue
@@ -159,7 +159,6 @@
             v-if="quest.status === $options.QuestStatuses.Done"
             class="card-quest__rating"
           >
-            <!--            {{ currentMark }}-->
             <star-rating
               v-if="userRole === $options.UserRole.WORKER ? quest.assignedWorkerId === userData.id : quest.userId === userData.id"
               class="card-quest__star"
