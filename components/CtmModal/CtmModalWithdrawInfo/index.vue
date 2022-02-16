276dd53e32b87b81900cbfafcb92d3d75ab69990
ghost1 <sonofagronom@gmail.com>
2022-02-16 11:13:30 +0700

    feat/22365/base-btn-selector in modal widthdrawInfo added data-selector="SHOW-NUMBER"

--- components/CtmModal/CtmModalWithdrawInfo/index.vue
+++ components/CtmModal/CtmModalWithdrawInfo/index.vue
@@ -14,6 +14,7 @@
           </div>
           <div
             class="header__subtitle"
+            data-selector="SHOW-NUMBER"
             @click="showNumber"
           >
             {{ getCardNumber }}
