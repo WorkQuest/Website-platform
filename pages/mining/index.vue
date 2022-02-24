4b38acfca93be074e4cb00c504fd99d35ca97b4e
ghost1 <sonofagronom@gmail.com>
2022-02-24 10:53:10 +0700

    fix/22896/fix-localization-files merge eng localization fix

--- pages/mining/index.vue
+++ pages/mining/index.vue
@@ -130,7 +130,7 @@ export default {
       return [
         {
           name: this.$t('mining.agreement'),
-          size: this.$tc('meta.kb', { count: '47' }),
+          size: this.$tc('meta.kb', '47'),
           url: '/docs/agreement.pdf',
         },
       ];
