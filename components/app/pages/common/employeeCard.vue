f1acb51aacf39f26e2ca40a5dabac7e141602b02
vseravno <vseravno2019@gmail.com>
2022-03-18 13:59:30 +0300

    add border

--- components/app/pages/common/employeeCard.vue
+++ components/app/pages/common/employeeCard.vue
@@ -2,6 +2,7 @@
   <div
     class="card"
     :data-selector="`COMPONENT-EMPLOYEE-CARD-${user.id}`"
+    :style="raiseViews"
     @click="$emit('click')"
   >
     <div class="card__header">
@@ -84,6 +85,30 @@ export default {
         string += `${i ? ', ' : ''}${name}`;
       });
       return string;
+    },
+    raiseViews() {
+      try {
+        switch (this.user.raiseView.type) {
+          case 0: {
+            return 'box-shadow: 0 0 10px 2px rgba(246, 207, 0, 0.3)';
+          }
+          case 1: {
+            return 'box-shadow: 0 0 10px 2px rgba(246, 207, 0, 0.3)';
+          }
+          case 2: {
+            return 'box-shadow: 0 0 10px 2px rgba(187,192,199, 0.3)';
+          }
+          case 3: {
+            return 'box-shadow: 3 0 0 10px 2px rgba(183,151,104, 0.3)';
+          }
+          default: {
+            console.log(this.user.raiseView.type);
+            return 'box-shadow: -1px 1px 8px 0px rgba(34, 60, 80, 0.2)';
+          }
+        }
+      } catch (e) {
+        return 'box-shadow: -1px 1px 8px 0px rgba(34, 60, 80, 0.2)';
+      }
     },
   },
   methods: {
