65d74fb7f7a2d55e6e891b58f9eeeb91c3e64e25
ghost1 <sonofagronom@gmail.com>
2022-03-24 15:59:40 +0700

    fix/24972/curr-lang-in-store update kyc page

--- pages/sumsub.vue
+++ pages/sumsub.vue
@@ -1,7 +1,28 @@
 <template>
-  <div class="main">
-    <div class="main__body">
-      <div id="sumsub-websdk-container" />
+  <div class="kyc">
+    <div class="kyc__body">
+      <div class="kyc__header">
+        {{ $t('kyc.KYC') }}
+      </div>
+      <div class="kyc__content">
+        <div
+          v-if="statusKYC === $options.SumSubStatuses.NOT_VERIFIED"
+          id="sumsub-websdk-container"
+        />
+        <div
+          v-if="statusKYC === $options.SumSubStatuses.VERIFIED"
+          class="kyc__verified"
+        >
+          <img
+            class="kyc__image"
+            src="~/assets/img/ui/questAgreed.svg"
+            alt=""
+          >
+          <div class="kyc__text">
+            {{ $t('kyc.alreadyVerified') }}
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -9,63 +30,106 @@
 <script>
 import snsWebSdk from '@sumsub/websdk';
 import { mapGetters } from 'vuex';
+import { SumSubStatuses } from '~/utils/enums';
 
 export default {
-  name: 'Sumsub',
-  data() {
-    return {};
-  },
+  name: 'KYC',
+  SumSubStatuses,
   computed: {
     ...mapGetters({
       accessToken: 'sumsub/getSumSubBackendToken',
       userData: 'user/getUserData',
+      statusKYC: 'user/getStatusKYC',
+      currentLocale: 'user/getCurrentLang',
     }),
+  },
+  watch: {
+    async currentLocale() {
+      await this.initSumSub();
+    },
   },
   async mounted() {
     this.SetLoader(true);
-    await this.createAccessToken();
-    this.initSumSub();
+    await this.initSumSub();
     this.SetLoader(false);
   },
   methods: {
-    async createAccessToken() {
-      await this.$store.dispatch('sumsub/createAccessTokenBackend', {
-        userId: this.accessToken.userId,
-      });
-    },
-    initSumSub() {
-      const accessToken = this.accessToken.token;
-      const applicantEmail = this.userData.email;
-      const snsWebSdkInstance = snsWebSdk.Builder('https://test-api.sumsub.com', 'basic-kyc')
-        .withAccessToken(accessToken, () => {})
-        .withConf({
-          lang: 'en',
-          email: applicantEmail,
-          onError: (error) => {
-            console.log('WebSDK onError', error);
-          },
-        }).build();
+    async initSumSub() {
+      if (this.statusKYC === SumSubStatuses.VERIFIED) return;
+      const { email } = this.userData;
+      try {
+        await this.$store.dispatch('sumsub/createAccessTokenBackend', { userId: this.accessToken.userId });
+        const accessToken = this.accessToken.token;
+        const snsWebSdkInstance = snsWebSdk.Builder('https://test-api.sumsub.com', 'basic-kyc')
+          .withAccessToken(accessToken, () => {
+          })
+          .withConf({
+            lang: this.currentLocale,
+            email,
+            onError: (error) => {
+              console.log('WebSDK onError', error);
+            },
+          })
+          .build();
 
-      snsWebSdkInstance.launch('#sumsub-websdk-container');
+        snsWebSdkInstance.launch('#sumsub-websdk-container');
+      } catch (e) {
+        console.error('Error with snsWebSdkInstance. ', e);
+      }
     },
   },
 };
 </script>
 
 <style lang="scss" scoped>
-.main {
+.kyc {
   @include main;
-  &-white {
-   @include main;
-   background: $white;
-   background: #FFFFFF;
-   margin: 0 0 20px 0;
-   border-radius: 6px;
-   justify-content: center;
- }
+  @include text-simple;
+
   &__body {
-   max-width: 1180px;
-   height: 100%;
- }
+    max-width: 1180px;
+    height: 100%;
+  }
+
+  &__header {
+    font-weight: 600;
+    font-size: 28px;
+    line-height: 36px;
+    color: $black800;
+    margin: 30px 0 20px;
+  }
+
+  &__image {
+    height: 100px;
+    width: 100px;
+  }
+
+  &__text {
+    margin-top: 10px;
+  }
+
+  &__verified {
+    padding: 20px 0 20px 0;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+  }
+
+  &__content {
+    display: flex;
+    height: calc(100% - 86px);
+    background: $white;
+    border-radius: 6px;
+    justify-content: center;
+    align-items: center;
+  }
+}
+
+@include _767 {
+  .kyc {
+    &__header {
+      margin-left: 15px;
+    }
+  }
 }
 </style>
