f00aa8573ea739208c975ba43f15f38665694096
matykoks <matyko.ks@gmail.com>
2022-03-28 18:01:30 +0700

    rollback z-index for loader

--- components/ui/Loader/index.vue
+++ components/ui/Loader/index.vue
@@ -70,18 +70,21 @@ export default {
   right: 0;
   left: 0;
   background: rgba(#000000, .5);
-  z-index: 1000000;
+  z-index: 100000;
   align-items: center;
   justify-content: center;
   display: flex;
+
   &_hider {
     background: #F7F8FA;
   }
+
   &__body {
     display: flex;
     flex-direction: column;
     align-items: center;
   }
+
   &__modal {
     width: 200px;
     position: relative;
@@ -94,6 +97,7 @@ export default {
     align-items: center;
     justify-content: center;
   }
+
   &__text {
     font-family: 'Inter', sans-serif;
     font-size: 23px;
@@ -135,33 +139,73 @@ export default {
   animation: sk-chase-dot-before 2.0s infinite ease-in-out both;
 }
 
-.sk-chase-dot:nth-child(1) { animation-delay: -1.1s; }
-.sk-chase-dot:nth-child(2) { animation-delay: -1.0s; }
-.sk-chase-dot:nth-child(3) { animation-delay: -0.9s; }
-.sk-chase-dot:nth-child(4) { animation-delay: -0.8s; }
-.sk-chase-dot:nth-child(5) { animation-delay: -0.7s; }
-.sk-chase-dot:nth-child(6) { animation-delay: -0.6s; }
-.sk-chase-dot:nth-child(1):before { animation-delay: -1.1s; }
-.sk-chase-dot:nth-child(2):before { animation-delay: -1.0s; }
-.sk-chase-dot:nth-child(3):before { animation-delay: -0.9s; }
-.sk-chase-dot:nth-child(4):before { animation-delay: -0.8s; }
-.sk-chase-dot:nth-child(5):before { animation-delay: -0.7s; }
-.sk-chase-dot:nth-child(6):before { animation-delay: -0.6s; }
+.sk-chase-dot:nth-child(1) {
+  animation-delay: -1.1s;
+}
+
+.sk-chase-dot:nth-child(2) {
+  animation-delay: -1.0s;
+}
+
+.sk-chase-dot:nth-child(3) {
+  animation-delay: -0.9s;
+}
+
+.sk-chase-dot:nth-child(4) {
+  animation-delay: -0.8s;
+}
+
+.sk-chase-dot:nth-child(5) {
+  animation-delay: -0.7s;
+}
+
+.sk-chase-dot:nth-child(6) {
+  animation-delay: -0.6s;
+}
+
+.sk-chase-dot:nth-child(1):before {
+  animation-delay: -1.1s;
+}
+
+.sk-chase-dot:nth-child(2):before {
+  animation-delay: -1.0s;
+}
+
+.sk-chase-dot:nth-child(3):before {
+  animation-delay: -0.9s;
+}
+
+.sk-chase-dot:nth-child(4):before {
+  animation-delay: -0.8s;
+}
+
+.sk-chase-dot:nth-child(5):before {
+  animation-delay: -0.7s;
+}
+
+.sk-chase-dot:nth-child(6):before {
+  animation-delay: -0.6s;
+}
 
 @keyframes sk-chase {
-  100% { transform: rotate(360deg); }
+  100% {
+    transform: rotate(360deg);
+  }
 }
 
 @keyframes sk-chase-dot {
-  80%, 100% { transform: rotate(360deg); }
+  80%, 100% {
+    transform: rotate(360deg);
+  }
 }
 
 @keyframes sk-chase-dot-before {
   50% {
     transform: scale(0.4);
-  } 100%, 0% {
-      transform: scale(1.0);
-    }
+  }
+  100%, 0% {
+    transform: scale(1.0);
+  }
 }
 
 </style>
