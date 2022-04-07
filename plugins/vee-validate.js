7def58afcd45d6968019eef0a5d02572b50f5d3a
Виноградов Алексей <vseravno2019@gmail.com>
2022-04-07 17:23:57 +0700

    fix(25607): divided the logic of the rules

--- plugins/vee-validate.js
+++ plugins/vee-validate.js
@@ -210,12 +210,15 @@ extend('percent', {
 });
 extend('greaterThanZero', {
   validate(value) {
-    if (((value.toString().includes('.')) ? (value.toString().split('.').pop().length) : (0)) < 3) {
-      return (value > 0);
-    }
-    return false;
+    return (value > 0);
   },
-  message: 'Please enter correct {_field_}',
+  message: "Please enter correct {_field_}, can't be zero",
+});
+extend('notMoreDecimalPlaces', {
+  validate(value) {
+    return (((value.toString().includes('.')) ? (value.toString().split('.').pop().length) : (0)) < 3);
+  },
+  message: 'Please enter correct {_field_}, no more than 2 decimal places',
 });
 extend('min_percent', {
   validate(value, { min }) {
