be3a9a5b19c3614c67c05518752c5f013bc15437
massimez <massi.mez@ya.ru>
2022-04-19 12:50:54 +0700

    feature/26916 Refactor getChartData

--- store/mining/getters.js
+++ store/mining/getters.js
@@ -3,46 +3,26 @@ export default {
   getChartData: (state) => {
     const chartDataAsc = [...state.chartData].sort((a, b) => (a.date > b.date ? 1 : 0));
     const lastElementIndex = chartDataAsc.length - 1;
-    const chartData = chartDataAsc.map((el, currentIndex) => {
+    return chartDataAsc.map((el, currentIndex) => {
       const currentDayIndex = lastElementIndex - currentIndex;
       const dayToDisplay = window.$nuxt.$root.$moment().utc(false).subtract(currentDayIndex, 'days');
       let n = 0;
-      let isFound = false;
-      while (n <= lastElementIndex && !isFound) {
+      while (n <= lastElementIndex) {
         const position = lastElementIndex - n;
         const currentDayInTheChart = window.$nuxt.$root.$moment(chartDataAsc[position].date * 1000).utc(false);
-        if (window.$nuxt.$root.$moment(dayToDisplay).isSameOrAfter(currentDayInTheChart, 'days') && !isFound) {
-          isFound = true;
+        if (window.$nuxt.$root.$moment(dayToDisplay).isSameOrAfter(currentDayInTheChart, 'days')) {
           return {
+            ...chartDataAsc[position],
             date: dayToDisplay,
-            reserve0: chartDataAsc[position].reserve0,
-            reserve1: chartDataAsc[position].reserve1,
-            totalSupply: chartDataAsc[position].totalSupply,
-            reserveUSD: chartDataAsc[position].reserveUSD,
-            dailyVolumeToken0: chartDataAsc[position].dailyVolumeToken0,
-            dailyVolumeToken1: chartDataAsc[position].dailyVolumeToken1,
-            dailyVolumeUSD: chartDataAsc[position].dailyVolumeUSD,
-            dailyTxns: chartDataAsc[position].dailyTxns,
           };
         }
         n += 1;
       }
-      if (!isFound) {
-        return {
-          date: dayToDisplay,
-          reserve0: chartDataAsc[0].reserve0,
-          reserve1: chartDataAsc[0].reserve1,
-          totalSupply: chartDataAsc[0].totalSupply,
-          reserveUSD: chartDataAsc[0].reserveUSD,
-          dailyVolumeToken0: chartDataAsc[0].dailyVolumeToken0,
-          dailyVolumeToken1: chartDataAsc[0].dailyVolumeToken1,
-          dailyVolumeUSD: chartDataAsc[0].dailyVolumeUSD,
-          dailyTxns: chartDataAsc[0].dailyTxns,
-        };
-      }
-      return {};
+      return {
+        ...chartDataAsc[0],
+        date: dayToDisplay,
+      };
     });
-    return chartData;
   },
   getTotalLiquidityUSD: (state) => state.totalLiquidityUSD,
 };
