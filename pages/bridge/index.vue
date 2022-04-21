54ca4185de38a0cce42480346b75f7fefc3694cf
AlexBessmelcev <alexbessmelcev@gmail.com>
2022-04-21 16:32:35 +0700

    fix/26758/styles_fix bridge and guest

--- pages/bridge/index.vue
+++ pages/bridge/index.vue
@@ -508,7 +508,7 @@ export default {
         display: grid;
         grid-template-columns: 1fr 34px 1fr;
         gap: 20px;
-        align-items: center;
+        align-items: end;
         padding: 20px;
 
         img {
@@ -677,12 +677,6 @@ export default {
     }
     &__content {
       grid-template-rows: auto;
-
-      .info-block {
-        &__swap-cont {
-          grid-template-columns: 1fr 24px 1fr;
-        }
-      }
     }
   }
 
