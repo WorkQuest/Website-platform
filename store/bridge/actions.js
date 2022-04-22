687aef3afcba4c6f936c5d15c84083cb0604dc59
AlexanderBess <78302587+AlexanderBess@users.noreply.github.com>
2022-04-22 12:02:24 +0700

    fix/22729/bridge_web-socket fix bridge/actions subscribe logic

--- store/bridge/actions.js
+++ store/bridge/actions.js
@@ -168,6 +168,7 @@ export default {
     try {
       await this.$wsNotifs.subscribe(`${Path.NOTIFICATIONS}${Path.BRIDGE}/${userAddress}`, async (msg) => {
         const swaps = JSON.parse(JSON.stringify(getters.getSwaps));
+        let swapsCount = JSON.parse(JSON.stringify(getters.getSwapsCount));
         const {
           event, signData, transactionHash, returnValues: {
             amount, chainFrom, chainTo, sender, timestamp,
@@ -190,8 +191,9 @@ export default {
             recipient: sender,
             status: true,
           });
-        }
-        if (event === BridgeEvents.SWAP_REDEEMED) {
+          const { result: { count } } = await this.$axios.$get(`/v1/bridge/recipient/${userAddress}/swaps`);
+          swapsCount = count;
+        } else {
           // eslint-disable-next-line array-callback-return
           swaps.some((item) => {
             if (item.nonce === +msg.data.returnValues.nonce) {
@@ -200,8 +202,7 @@ export default {
             }
           });
         }
-        const { result: { count } } = await this.$axios.$get(`/v1/bridge/recipient/${userAddress}/swaps`);
-        commit('setSwapsData', { count, swaps });
+        commit('setSwapsData', { count: swapsCount, swaps });
       });
     } catch (err) {
       console.error('subscribeToBridgeEvents err', err);
