c6235872549bf6c6c6b5124714b8e2b867dc5e27
ghost1 <sonofagronom@gmail.com>
2022-05-12 11:41:37 +0700

    feature/26409/adding-missing-notifications-test refactoring

--- pages/settings.vue
+++ pages/settings.vue
@@ -52,7 +52,9 @@ import VerificationCard from '~/components/app/pages/settings/VerificationCard.v
 import Profile from '~/components/app/pages/settings/Profile.vue';
 import Skills from '~/components/app/pages/settings/Skills.vue';
 import Advanced from '~/components/app/pages/settings/Advanced.vue';
-import { UserRole, WorkplaceIndex, Path, RatingStatus } from '~/utils/enums';
+import {
+  UserRole, WorkplaceIndex, Path, RatingStatus,
+} from '~/utils/enums';
 import { LocalNotificationAction } from '~/utils/notifications-enum';
 
 export default {
