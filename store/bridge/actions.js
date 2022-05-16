e4c68248d59ab94cc7537ddaff7a1840bb5df47c
AlexanderBess <78302587+AlexanderBess@users.noreply.github.com>
2022-05-16 20:02:33 +0700

    feature/27908/USDT_for_bridge delete unused code

--- store/bridge/actions.js
+++ store/bridge/actions.js
@@ -1,6 +1,6 @@
 import BigNumber from 'bignumber.js';
 
-import { Path, tokenMap, TokenSymbols } from '~/utils/enums';
+import { Path, TokenSymbols } from '~/utils/enums';
 import {
   BlockchainByIndex, BridgeAddresses, BridgeEvents, SwapAddresses,
 } from '~/utils/сonstants/bridge';
