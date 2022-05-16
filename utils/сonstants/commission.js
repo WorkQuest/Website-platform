bcccd01da3ad3b6987e59e3e0e770f60c4183068
JuSTeRFeeD <justerfeedy@gmail.com>
2022-05-16 15:43:44 +0700

    feature/27907/buy-wqt-modal commission

--- utils/сonstants/commission.js
+++ utils/сonstants/commission.js
@@ -2,4 +2,4 @@
 export const CommissionForCreatingAQuest = Object.freeze(0.025);
 
 // wallet page buy wqt
-export const WQTBuyCommission = 0;
+export const WQTBuyCommission = 0.01; // will calc final value us: 1 - WQTBuyCommission
