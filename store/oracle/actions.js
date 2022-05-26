1689c6ca694b87f0db8ed7466c452380bd334cad
JuSTeRFeeD <justerfeedy@gmail.com>
2022-05-26 17:45:03 +0700

    feature/30180/secrets-fixes oracle env fix

--- store/oracle/actions.js
+++ store/oracle/actions.js
@@ -4,6 +4,8 @@ import {
 } from '~/utils/wallet';
 import { WQOracle } from '~/abi';
 
+import ENV from '~/utils/adresses/index';
+
 export default {
   async getCurrentPrices({ commit }) {
     try {
@@ -42,7 +44,7 @@ export default {
       r: res.r,
       s: res.s,
     };
-    const { gas, gasPrice } = await getGasPrice(WQOracle, this.ENV.WORKNET_ORACLE, 'setTokenPriceUSD', [...Object.values(params), symbol]);
+    const { gas, gasPrice } = await getGasPrice(WQOracle, ENV.WORKNET_ORACLE, 'setTokenPriceUSD', [...Object.values(params), symbol]);
     if (gas && gasPrice) {
       await setTokenPrice({ currency: symbol }, { gasPrice, gas, ...params });
     }
@@ -58,7 +60,7 @@ export default {
       timestamp, v, r, s, prices, symbols,
     };
 
-    const { gas, gasPrice } = await getGasPrice(WQOracle, this.ENV.WORKNET_ORACLE, 'setTokenPricesUSD', Object.values(params));
+    const { gas, gasPrice } = await getGasPrice(WQOracle, ENV.WORKNET_ORACLE, 'setTokenPricesUSD', Object.values(params));
     if (gas && gasPrice) {
       await setTokenPrices({
         gasPrice,
