242781a1c1e1701b244f5e3bab3edcb13a9a534a
ghost1 <sonofagronom@gmail.com>
2022-05-27 15:26:19 +0700

    feature/30032/resend-email added clearTimer, if user login

--- pages/sign-in.vue
+++ pages/sign-in.vue
@@ -405,7 +405,7 @@ export default {
         });
         this.SetLoader(false);
         return;
-      }
+      } this.clearTimer();
 
       // Redirect to confirm account
       if (confirmToken) {
