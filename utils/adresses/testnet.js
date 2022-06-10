b37f4d2901a52ee20740805af64198dd9bd16693
matykoks <matyko.ks@gmail.com>
2022-06-10 13:09:37 +0700

    fixed url foe testnet node, added branch testnet in isProd

--- utils/adresses/testnet.js
+++ utils/adresses/testnet.js
@@ -3,7 +3,7 @@ export default {
   // GMAPKEY: process.env.GMAPKEY,
   // SECRET_SUMSUB: process.env.SECRET_SUMSUB,
 
-  WQ_PROVIDER: 'https://test-gate-01.workquest.co/',
+  WQ_PROVIDER: 'https://testnet-gate.workquest.co/rpc/',
   WQ_EXPLORER: 'https://testnet-explorer.workquest.co/api/v1',
   WQ_ORACLE_URL: 'https://testnet-oracle.workquest.co/api/v1',
 
