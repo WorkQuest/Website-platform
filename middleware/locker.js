6400b808d67b388d0fd55398bf84ec2c592780ad
matykoks <matyko.ks@gmail.com>
2022-06-28 14:42:01 +0700

    fixes locker for default layout

--- middleware/locker.js
+++ middleware/locker.js
@@ -1,9 +1,9 @@
 import { Path } from '~/utils/enums';
-import { LockedPages } from '~/utils/locker-data';
+import { IS_PLUG, LockedPages } from '~/utils/locker-data';
 
 // eslint-disable-next-line func-names
 export default async function ({ redirect, route }) {
-  if (LockedPages.includes(route.name)) {
+  if (IS_PLUG && LockedPages.includes(route.name)) {
     redirect(Path.ROOT);
   }
   return null;
