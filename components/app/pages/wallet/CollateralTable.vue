7dc3b2a0575205ee2e3853954ce15d5f697ae185
matykoks <matyko.ks@gmail.com>
2022-07-27 15:55:49 +0600

    fixed url for collateral

--- components/app/pages/wallet/CollateralTable.vue
+++ components/app/pages/wallet/CollateralTable.vue
@@ -95,7 +95,7 @@
               <div>{{ row._price }}</div>
               <a
                 class="item__hash"
-                :href="`${$options.ExplorerUrl}/tx/${item.transactionHash}`"
+                :href="`${$options.ExplorerUrl}/tx/${row.transactionHash}`"
                 target="_blank"
               >
                 {{ CutTxn( row.transactionHash) }}
