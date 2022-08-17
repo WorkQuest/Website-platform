b89c7e1c046f70ed3a47ab06d095bfeff6090516
JuSTeRFeeD <justerfeedy@gmail.com>
2022-08-17 16:00:34 +0700

    update/33327/delete-chat review fix

--- components/app/pages/messages_id/messagesList.vue
+++ components/app/pages/messages_id/messagesList.vue
@@ -234,19 +234,14 @@ export default {
       chats: 'chat/getChats',
       currChatIsUnread: 'chat/currChatIsUnread',
     }),
-    MessageAction() {
-      return MessageAction;
-    },
     MessageType() {
       return MessageType;
     },
     chatMembers() {
       const res = {};
       if (!this.currChat?.members) return res;
-      // eslint-disable-next-line no-restricted-syntax
-      for (const item of this.currChat.members) {
-        res[item.userId] = item;
-      }
+      // eslint-disable-next-line no-return-assign
+      this.currChat.members.forEach((item) => res[item.userId] = item);
       return res;
     },
   },
