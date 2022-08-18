606c3825717c98ee8e7e56343ec86e6caf6213ff
JuSTeRFeeD <justerfeedy@gmail.com>
2022-08-18 11:31:45 +0700

    feature/error-layout added temp error layer

---
+++ layouts/error.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="error">
+    <h1>Something was wrong....</h1>
+    <div>
+      Click there to back
+      <base-btn nuxt-link="my">
+        Back
+      </base-btn>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ErrorLayout',
+};
+</script>
+
+<style scoped lang="scss">
+.error {
+  margin-top: 20px;
+}
+.h1 {
+  font-size: 32px;
+  font-weight: 700;
+}
+</style>
