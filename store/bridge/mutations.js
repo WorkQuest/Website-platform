beded6c8139f4726aa6e5043fb640e61050930f0
JuSTeRFeeD <justerfeedy@gmail.com>
2022-08-29 10:28:24 +0700

    feature/35222/USDC-on-bridge redeemed mutation after sending tx

--- store/bridge/mutations.js
+++ store/bridge/mutations.js
@@ -7,6 +7,15 @@ export default {
     state.swaps = [];
     state.swapsCount = null;
   },
+  setRedeemed(state, hash) {
+    const index = state.swaps.findIndex((item) => item.transactionHash === hash);
+    if (index !== -1) {
+      const swaps = JSON.parse(JSON.stringify(state.swaps));
+      swaps[index].canRedeemed = false;
+      swaps[index].status = false;
+      state.swaps = swaps;
+    }
+  },
   setToken(state, payload) {
     state.token = { ...payload };
   },
