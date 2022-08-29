5db4236c88a7480559774bf736a67a6bc794736a
JuSTeRFeeD <justerfeedy@gmail.com>
2022-08-29 12:59:25 +0700

    update/30274/report-notifs report actions notifications

--- pages/notifications/index.vue
+++ pages/notifications/index.vue
@@ -89,7 +89,7 @@
                 @click="tryRemoveNotification($event, notification)"
               >
               <base-btn
-                v-if="notification.params"
+                v-if="notification.params && notification.params.path"
                 mode="outline"
                 :link="notification.params.isExternalLink ? `${notification.params.externalBase}${notification.params.path}` : ''"
                 class="button__view"
@@ -243,7 +243,7 @@ export default {
       this.checkUnseenNotifs();
     },
     goToEvent(notification, isRefBtn) {
-      if (!notification.params || (document.body.offsetWidth > 767 && !isRefBtn)) return;
+      if (!notification.params?.path || (document.body.offsetWidth > 767 && !isRefBtn)) return;
       const { path } = notification.params;
       if (notification.params.isExternalLink) {
         window.open(notification.params.externalBase + path, '_blank');
