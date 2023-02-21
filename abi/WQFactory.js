70232fa2bfbd8418669aa4180bd11dd664326a67
Serhii Lukashov <serhii.lukashov@pvls.tech>
2023-02-21 21:24:11 +0100

    Updated abi

--- abi/WQFactory.js
+++ abi/WQFactory.js
@@ -2,7 +2,7 @@ const WQFactory = [
   {
     inputs: [],
     stateMutability: 'nonpayable',
-    type: 'constructor',
+    type: 'constructor'
   },
   {
     anonymous: false,
@@ -11,17 +11,17 @@ const WQFactory = [
         indexed: false,
         internalType: 'address',
         name: 'previousAdmin',
-        type: 'address',
+        type: 'address'
       },
       {
         indexed: false,
         internalType: 'address',
         name: 'newAdmin',
-        type: 'address',
-      },
+        type: 'address'
+      }
     ],
     name: 'AdminChanged',
-    type: 'event',
+    type: 'event'
   },
   {
     anonymous: false,
@@ -30,11 +30,11 @@ const WQFactory = [
         indexed: true,
         internalType: 'address',
         name: 'beacon',
-        type: 'address',
-      },
+        type: 'address'
+      }
     ],
     name: 'BeaconUpgraded',
-    type: 'event',
+    type: 'event'
   },
   {
     anonymous: false,
@@ -43,11 +43,11 @@ const WQFactory = [
         indexed: false,
         internalType: 'uint8',
         name: 'version',
-        type: 'uint8',
-      },
+        type: 'uint8'
+      }
     ],
     name: 'Initialized',
-    type: 'event',
+    type: 'event'
   },
   {
     anonymous: false,
@@ -56,23 +56,23 @@ const WQFactory = [
         indexed: true,
         internalType: 'bytes32',
         name: 'role',
-        type: 'bytes32',
+        type: 'bytes32'
       },
       {
         indexed: true,
         internalType: 'bytes32',
         name: 'previousAdminRole',
-        type: 'bytes32',
+        type: 'bytes32'
       },
       {
         indexed: true,
         internalType: 'bytes32',
         name: 'newAdminRole',
-        type: 'bytes32',
-      },
+        type: 'bytes32'
+      }
     ],
     name: 'RoleAdminChanged',
-    type: 'event',
+    type: 'event'
   },
   {
     anonymous: false,
@@ -81,23 +81,23 @@ const WQFactory = [
         indexed: true,
         internalType: 'bytes32',
         name: 'role',
-        type: 'bytes32',
+        type: 'bytes32'
       },
       {
         indexed: true,
         internalType: 'address',
         name: 'account',
-        type: 'address',
+        type: 'address'
       },
       {
         indexed: true,
         internalType: 'address',
         name: 'sender',
-        type: 'address',
-      },
+        type: 'address'
+      }
     ],
     name: 'RoleGranted',
-    type: 'event',
+    type: 'event'
   },
   {
     anonymous: false,
@@ -106,23 +106,23 @@ const WQFactory = [
         indexed: true,
         internalType: 'bytes32',
         name: 'role',
-        type: 'bytes32',
+        type: 'bytes32'
       },
       {
         indexed: true,
         internalType: 'address',
         name: 'account',
-        type: 'address',
+        type: 'address'
       },
       {
         indexed: true,
         internalType: 'address',
         name: 'sender',
-        type: 'address',
-      },
+        type: 'address'
+      }
     ],
     name: 'RoleRevoked',
-    type: 'event',
+    type: 'event'
   },
   {
     anonymous: false,
@@ -131,11 +131,11 @@ const WQFactory = [
         indexed: true,
         internalType: 'address',
         name: 'implementation',
-        type: 'address',
-      },
+        type: 'address'
+      }
     ],
     name: 'Upgraded',
-    type: 'event',
+    type: 'event'
   },
   {
     anonymous: false,
@@ -144,35 +144,35 @@ const WQFactory = [
         indexed: false,
         internalType: 'bytes32',
         name: 'jobHash',
-        type: 'bytes32',
+        type: 'bytes32'
       },
       {
         indexed: false,
         internalType: 'address',
         name: 'employer',
-        type: 'address',
+        type: 'address'
       },
       {
         indexed: false,
         internalType: 'address',
         name: 'workquest',
-        type: 'address',
+        type: 'address'
       },
       {
         indexed: false,
         internalType: 'uint256',
         name: 'createdAt',
-        type: 'uint256',
+        type: 'uint256'
       },
       {
         indexed: false,
         internalType: 'uint256',
         name: 'nonce',
-        type: 'uint256',
-      },
+        type: 'uint256'
+      }
     ],
     name: 'WorkQuestCreated',
-    type: 'event',
+    type: 'event'
   },
   {
     inputs: [],
@@ -181,11 +181,11 @@ const WQFactory = [
       {
         internalType: 'bytes32',
         name: '',
-        type: 'bytes32',
-      },
+        type: 'bytes32'
+      }
     ],
     stateMutability: 'view',
-    type: 'function',
+    type: 'function'
   },
   {
     inputs: [],
@@ -194,11 +194,11 @@ const WQFactory = [
       {
         internalType: 'bytes32',
         name: '',
-        type: 'bytes32',
-      },
+        type: 'bytes32'
+      }
     ],
     stateMutability: 'view',
-    type: 'function',
+    type: 'function'
   },
   {
     inputs: [],
@@ -207,11 +207,11 @@ const WQFactory = [
       {
         internalType: 'bytes32',
         name: '',
-        type: 'bytes32',
-      },
+        type: 'bytes32'
+      }
     ],
     stateMutability: 'view',
-    type: 'function',
+    type: 'function'
   },
   {
     inputs: [],
@@ -220,11 +220,11 @@ const WQFactory = [
       {
         internalType: 'bytes32',
         name: '',
-        type: 'bytes32',
-      },
+        type: 'bytes32'
+      }
     ],
     stateMutability: 'view',
-    type: 'function',
+    type: 'function'
   },
   {
     inputs: [],
@@ -233,11 +233,11 @@ const WQFactory = [
       {
         internalType: 'uint256',
         name: '',
-        type: 'uint256',
-      },
+        type: 'uint256'
+      }
     ],
     stateMutability: 'view',
-    type: 'function',
+    type: 'function'
   },
   {
     inputs: [],
@@ -246,11 +246,11 @@ const WQFactory = [
       {
         internalType: 'address payable',
         name: '',
-        type: 'address',
-      },
+        type: 'address'
+      }
     ],
     stateMutability: 'view',
-    type: 'function',
+    type: 'function'
   },
   {
     inputs: [],
@@ -259,11 +259,11 @@ const WQFactory = [
       {
         internalType: 'uint256',
         name: '',
-        type: 'uint256',
-      },
+        type: 'uint256'
+      }
     ],
     stateMutability: 'view',
-    type: 'function',
+    type: 'function'
   },
   {
     inputs: [],
@@ -272,172 +272,167 @@ const WQFactory = [
       {
         internalType: 'uint256',
         name: '',
-        type: 'uint256',
-      },
+        type: 'uint256'
+      }
     ],
     stateMutability: 'view',
-    type: 'function',
+    type: 'function'
   },
   {
     inputs: [
       {
         internalType: 'bytes32',
         name: 'role',
-        type: 'bytes32',
-      },
+        type: 'bytes32'
+      }
     ],
     name: 'getRoleAdmin',
     outputs: [
       {
         internalType: 'bytes32',
         name: '',
-        type: 'bytes32',
-      },
+        type: 'bytes32'
+      }
     ],
     stateMutability: 'view',
-    type: 'function',
+    type: 'function'
   },
   {
     inputs: [
       {
         internalType: 'address',
         name: 'employer',
-        type: 'address',
+        type: 'address'
       },
       {
         internalType: 'uint256',
         name: 'offset',
-        type: 'uint256',
+        type: 'uint256'
       },
       {
         internalType: 'uint256',
         name: 'limit',
-        type: 'uint256',
-      },
+        type: 'uint256'
+      }
     ],
     name: 'getWorkQuests',
     outputs: [
       {
         internalType: 'address[]',
         name: 'page',
-        type: 'address[]',
-      },
+        type: 'address[]'
+      }
     ],
     stateMutability: 'view',
-    type: 'function',
+    type: 'function'
   },
   {
     inputs: [
       {
         internalType: 'bytes32',
         name: 'role',
-        type: 'bytes32',
+        type: 'bytes32'
       },
       {
         internalType: 'address',
         name: 'account',
-        type: 'address',
-      },
+        type: 'address'
+      }
     ],
     name: 'grantRole',
     outputs: [],
     stateMutability: 'nonpayable',
-    type: 'function',
+    type: 'function'
   },
   {
     inputs: [
       {
         internalType: 'bytes32',
         name: 'role',
-        type: 'bytes32',
+        type: 'bytes32'
       },
       {
         internalType: 'address',
         name: 'account',
-        type: 'address',
-      },
+        type: 'address'
+      }
     ],
     name: 'hasRole',
     outputs: [
       {
         internalType: 'bool',
         name: '',
-        type: 'bool',
-      },
+        type: 'bool'
+      }
     ],
     stateMutability: 'view',
-    type: 'function',
+    type: 'function'
   },
   {
     inputs: [
       {
         internalType: 'uint256',
         name: '_feeEmployer',
-        type: 'uint256',
+        type: 'uint256'
       },
       {
         internalType: 'uint256',
         name: '_feeWorker',
-        type: 'uint256',
+        type: 'uint256'
       },
       {
         internalType: 'uint256',
         name: '_feeTx',
-        type: 'uint256',
+        type: 'uint256'
       },
       {
         internalType: 'address payable',
         name: '_feeReceiver',
-        type: 'address',
-      },
-      {
-        internalType: 'address payable',
-        name: '_pensionFund',
-        type: 'address',
+        type: 'address'
       },
       {
         internalType: 'address payable',
         name: '_referral',
-        type: 'address',
+        type: 'address'
       },
       {
         internalType: 'address',
-        name: '_wusd',
-        type: 'address',
-      },
+        name: '_usdt',
+        type: 'address'
+      }
     ],
     name: 'initialize',
     outputs: [],
     stateMutability: 'nonpayable',
-    type: 'function',
+    type: 'function'
   },
   {
     inputs: [
       {
         internalType: 'bytes32',
         name: 'jobHash',
-        type: 'bytes32',
+        type: 'bytes32'
       },
       {
         internalType: 'uint256',
         name: 'cost',
-        type: 'uint256',
+        type: 'uint256'
       },
       {
         internalType: 'uint256',
         name: 'deadline',
-        type: 'uint256',
+        type: 'uint256'
       },
       {
         internalType: 'uint256',
         name: 'nonce',
-        type: 'uint256',
-      },
+        type: 'uint256'
+      }
     ],
     name: 'newWorkQuest',
     outputs: [],
     stateMutability: 'nonpayable',
-    type: 'function',
+    type: 'function'
   },
   {
     inputs: [],
@@ -446,11 +441,11 @@ const WQFactory = [
       {
         internalType: 'address payable',
         name: '',
-        type: 'address',
-      },
+        type: 'address'
+      }
     ],
     stateMutability: 'view',
-    type: 'function',
+    type: 'function'
   },
   {
     inputs: [],
@@ -459,11 +454,11 @@ const WQFactory = [
       {
         internalType: 'bytes32',
         name: '',
-        type: 'bytes32',
-      },
+        type: 'bytes32'
+      }
     ],
     stateMutability: 'view',
-    type: 'function',
+    type: 'function'
   },
   {
     inputs: [],
@@ -472,245 +467,245 @@ const WQFactory = [
       {
         internalType: 'address payable',
         name: '',
-        type: 'address',
-      },
+        type: 'address'
+      }
     ],
     stateMutability: 'view',
-    type: 'function',
+    type: 'function'
   },
   {
     inputs: [
       {
         internalType: 'bytes32',
         name: 'role',
-        type: 'bytes32',
+        type: 'bytes32'
       },
       {
         internalType: 'address',
         name: 'account',
-        type: 'address',
-      },
+        type: 'address'
+      }
     ],
     name: 'renounceRole',
     outputs: [],
     stateMutability: 'nonpayable',
-    type: 'function',
+    type: 'function'
   },
   {
     inputs: [
       {
         internalType: 'bytes32',
         name: 'role',
-        type: 'bytes32',
+        type: 'bytes32'
       },
       {
         internalType: 'address',
         name: 'account',
-        type: 'address',
-      },
+        type: 'address'
+      }
     ],
     name: 'revokeRole',
     outputs: [],
     stateMutability: 'nonpayable',
-    type: 'function',
+    type: 'function'
   },
   {
     inputs: [
       {
         internalType: 'uint256',
         name: '_fee',
-        type: 'uint256',
-      },
+        type: 'uint256'
+      }
     ],
     name: 'setFeeEmployer',
     outputs: [],
     stateMutability: 'nonpayable',
-    type: 'function',
+    type: 'function'
   },
   {
     inputs: [
       {
         internalType: 'address payable',
         name: '_feeReceiver',
-        type: 'address',
-      },
+        type: 'address'
+      }
     ],
     name: 'setFeeReceiver',
     outputs: [],
     stateMutability: 'nonpayable',
-    type: 'function',
+    type: 'function'
   },
   {
     inputs: [
       {
         internalType: 'uint256',
         name: '_fee',
-        type: 'uint256',
-      },
+        type: 'uint256'
+      }
     ],
     name: 'setFeeTx',
     outputs: [],
     stateMutability: 'nonpayable',
-    type: 'function',
+    type: 'function'
   },
   {
     inputs: [
       {
         internalType: 'uint256',
         name: '_fee',
-        type: 'uint256',
-      },
+        type: 'uint256'
+      }
     ],
     name: 'setFeeWorker',
     outputs: [],
     stateMutability: 'nonpayable',
-    type: 'function',
+    type: 'function'
   },
   {
     inputs: [
       {
         internalType: 'address payable',
         name: '_pensionFund',
-        type: 'address',
-      },
+        type: 'address'
+      }
     ],
     name: 'setPensionFund',
     outputs: [],
     stateMutability: 'nonpayable',
-    type: 'function',
+    type: 'function'
   },
   {
     inputs: [
       {
         internalType: 'address payable',
         name: '_referral',
-        type: 'address',
-      },
+        type: 'address'
+      }
     ],
     name: 'setRefferal',
     outputs: [],
     stateMutability: 'nonpayable',
-    type: 'function',
+    type: 'function'
   },
   {
     inputs: [
       {
         internalType: 'address',
-        name: '_wusd',
-        type: 'address',
-      },
+        name: '_usdt',
+        type: 'address'
+      }
     ],
-    name: 'setWusd',
+    name: 'setUsdt',
     outputs: [],
     stateMutability: 'nonpayable',
-    type: 'function',
+    type: 'function'
   },
   {
     inputs: [
       {
         internalType: 'bytes4',
         name: 'interfaceId',
-        type: 'bytes4',
-      },
+        type: 'bytes4'
+      }
     ],
     name: 'supportsInterface',
     outputs: [
       {
         internalType: 'bool',
         name: '',
-        type: 'bool',
-      },
+        type: 'bool'
+      }
     ],
     stateMutability: 'view',
-    type: 'function',
+    type: 'function'
   },
   {
     inputs: [
       {
         internalType: 'address',
         name: 'newImplementation',
-        type: 'address',
-      },
+        type: 'address'
+      }
     ],
     name: 'upgradeTo',
     outputs: [],
     stateMutability: 'nonpayable',
-    type: 'function',
+    type: 'function'
   },
   {
     inputs: [
       {
         internalType: 'address',
         name: 'newImplementation',
-        type: 'address',
+        type: 'address'
       },
       {
         internalType: 'bytes',
         name: 'data',
-        type: 'bytes',
-      },
+        type: 'bytes'
+      }
     ],
     name: 'upgradeToAndCall',
     outputs: [],
     stateMutability: 'payable',
-    type: 'function',
+    type: 'function'
+  },
+  {
+    inputs: [],
+    name: 'usdt',
+    outputs: [
+      {
+        internalType: 'contract IERC20Upgradeable',
+        name: '',
+        type: 'address'
+      }
+    ],
+    stateMutability: 'view',
+    type: 'function'
   },
   {
     inputs: [
       {
         internalType: 'address',
         name: '',
-        type: 'address',
-      },
+        type: 'address'
+      }
     ],
     name: 'workquestValid',
     outputs: [
       {
         internalType: 'bool',
         name: '',
-        type: 'bool',
-      },
+        type: 'bool'
+      }
     ],
     stateMutability: 'view',
-    type: 'function',
+    type: 'function'
   },
   {
     inputs: [
       {
         internalType: 'address',
         name: '',
-        type: 'address',
+        type: 'address'
       },
       {
         internalType: 'uint256',
         name: '',
-        type: 'uint256',
-      },
+        type: 'uint256'
+      }
     ],
     name: 'workquests',
     outputs: [
       {
         internalType: 'address',
         name: '',
-        type: 'address',
-      },
+        type: 'address'
+      }
     ],
     stateMutability: 'view',
-    type: 'function',
-  },
-  {
-    inputs: [],
-    name: 'wusd',
-    outputs: [
-      {
-        internalType: 'contract IERC20Upgradeable',
-        name: '',
-        type: 'address',
-      },
-    ],
-    stateMutability: 'view',
-    type: 'function',
-  },
+    type: 'function'
+  }
 ];
 
 export default WQFactory;
