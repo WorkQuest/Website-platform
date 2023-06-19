8a53d21469abb03792cd577a32fc47c6e687dca6
denysYerem <denys.yeremenko@pvls.tech>
2023-06-19 23:10:51 +0200

    Add console logs for tracking transmitted data

--- store/bridge/actions.js
+++ store/bridge/actions.js
@@ -69,11 +69,6 @@ export default {
   async redeemSwap({ commit }, {
     signData, chainTo, provider, accountAddress,
   }) {
-    console.log(`signData${signData}`);
-    console.log(`chainTo${chainTo}`);
-    console.log(`provider${provider}`);
-    console.log(`accountAddress${accountAddress}`);
-
     try {
       showToast('Redeeming', 'Redeem...', 'success');
 
